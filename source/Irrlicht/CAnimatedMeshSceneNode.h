c729f356f0b47d7aaa23b9cc75725b93e1b453c2
bitplane <bitplane@dfc29bdd-3216-0410-991c-e03cc46cb475>
2007-06-09 03:14:17 +0000

    renamed IAnimatedMeshSceneNode::getAbsoluteTransformation to getMD3TagTransformation, it was causing problems for users accessing ISceneNode::getAbsoluteTransformation
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@701 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CAnimatedMeshSceneNode.h
+++ source/Irrlicht/CAnimatedMeshSceneNode.h
@@ -127,7 +127,7 @@ namespace scene
 
 		// returns the absolute transformation for a special MD3 Tag if the mesh is a md3 mesh,
 		// or the absolutetransformation if it's a normal scenenode
-		const SMD3QuaterionTag& getAbsoluteTransformation( const core::stringc & tagname);
+		const SMD3QuaterionTag& getMD3TagTransformation( const core::stringc & tagname);
 
 		//! updates the absolute position based on the relative and the parents position
 		virtual void updateAbsolutePosition();
