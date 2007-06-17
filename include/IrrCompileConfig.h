3c3b14cc42ffdd7dade03960313d9e0dbca66ff3
irrlicht <irrlicht@dfc29bdd-3216-0410-991c-e03cc46cb475>
2007-06-17 07:48:20 +0000

    updated irrlicht version to 1.3.1
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@714 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IrrCompileConfig.h
+++ include/IrrCompileConfig.h
@@ -6,7 +6,7 @@
 #define __IRR_COMPILE_CONFIG_H_INCLUDED__
 
 //! Irrlicht SDK Version
-#define IRRLICHT_SDK_VERSION "1.3"
+#define IRRLICHT_SDK_VERSION "1.3.1"
 
 //! The defines for different operating system are:
 //! _IRR_XBOX_PLATFORM_ for XBox
