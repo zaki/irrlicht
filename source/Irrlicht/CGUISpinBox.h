d381a6eadc9dc48d8cbc7726c1a2f8f8fbdd54af
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2007-07-01 10:16:39 +0000

    Fixed reorder warning and indentation.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@748 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CGUISpinBox.h
+++ source/Irrlicht/CGUISpinBox.h
@@ -10,8 +10,8 @@ namespace irr
 {
 namespace gui
 {
-    class IGUIEditBox;
-    class IGUIButton;
+	class IGUIEditBox;
+	class IGUIButton;
 
 	class CGUISpinBox : public IGUISpinBox
 	{
@@ -24,20 +24,20 @@ namespace gui
 		//! destructor
 		~CGUISpinBox();
 
-        //! Access the edit box used in the spin control
+		//! Access the edit box used in the spin control
 		/** \param enable: If set to true, the override color, which can be set
-		 with IGUIEditBox::setOverrideColor is used, otherwise the
-		 EGDC_BUTTON_TEXT color of the skin. */
+		with IGUIEditBox::setOverrideColor is used, otherwise the
+		EGDC_BUTTON_TEXT color of the skin. */
 		virtual IGUIEditBox* getEditBox();
 
-        //! set the current value of the spinbox
+		//! set the current value of the spinbox
 		/** \param val: value to be set in the spinbox */
 		virtual void setValue(f32 val);
 
-        //! Get the current value of the spinbox
+		//! Get the current value of the spinbox
 		virtual f32 getValue();
 
-        //! set the range of values which can be used in the spinbox
+		//! set the range of values which can be used in the spinbox
 		/** \param min: minimum value
 		\param max: maximum value */
 		virtual void setRange(f32 min, f32 max);
@@ -58,11 +58,11 @@ namespace gui
 		//! called if an event happened.
 		virtual bool OnEvent(SEvent event);
 
-        //! Sets the new caption of the element
-        virtual void setText(const wchar_t* text);
+		//! Sets the new caption of the element
+		virtual void setText(const wchar_t* text);
 
-        //! Returns caption of this element.
-        virtual const wchar_t* getText();
+		//! Returns caption of this element.
+		virtual const wchar_t* getText();
 
 		//! Sets the number of decimal places to display.
 		/** \param places: The number of decimal places to display, use -1 to reset */
@@ -74,20 +74,21 @@ namespace gui
 		//! Reads attributes of the element
 		virtual void deserializeAttributes(io::IAttributes* in, io::SAttributeReadWriteOptions* options);
 
-    protected:
-        virtual void verifyValueRange();
+	protected:
+		virtual void verifyValueRange();
+
+		IGUIEditBox * EditBox;
+		IGUIButton * ButtonSpinUp;
+		IGUIButton * ButtonSpinDown;
+		f32 StepSize;
+		f32 RangeMin;
+		f32 RangeMax;
 
 		core::stringw FormatString;
 		s32 DecimalPlaces;
-
-        IGUIEditBox * EditBox;
-        IGUIButton * ButtonSpinUp;
-        IGUIButton * ButtonSpinDown;
-        f32 StepSize;
-        f32 RangeMin;
-        f32 RangeMax;
 	};
 } // end namespace gui
 } // end namespace irr
 
 #endif // __C_GUI_SPIN_BOX_H_INCLUDED__
+
