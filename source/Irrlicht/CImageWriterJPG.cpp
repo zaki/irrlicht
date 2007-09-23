71b4579be16c6c428b7cd711b698741e546952bf
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2007-09-23 18:54:07 +0000

    Some image writer fixes for proper directx screenshots.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@998 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CImageWriterJPG.cpp
+++ source/Irrlicht/CImageWriterJPG.cpp
@@ -186,7 +186,7 @@ bool CImageWriterJPG::writeImage(io::IWriteFile *file, IImage *input,u32 quality
 	switch( input->getColorFormat () )
 	{
 		case ECF_R8G8B8:	format = CColorConverter::convert_R8G8B8toR8G8B8; break;
-		case ECF_A8R8G8B8:	format = CColorConverter::convert_A8R8G8B8toB8G8R8; break;
+		case ECF_A8R8G8B8:	format = CColorConverter::convert_A8R8G8B8toR8G8B8; break;
 		case ECF_A1R5G5B5:	format = CColorConverter::convert_A1R5G5B5toB8G8R8; break;
 		case ECF_R5G6B5:	format = CColorConverter::convert_R5G6B5toR8G8B8; break;
 	}
