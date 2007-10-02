7ce4f2517f5c0f90324d6cb8341c37617d477de5
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2007-10-02 22:41:05 +0000

    Some documentation updates.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@1020 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/ITriangleSelector.h
+++ include/ITriangleSelector.h
@@ -19,10 +19,10 @@ namespace scene
 //! Interface to return triangles with specific properties.
 /** Every ISceneNode may have a triangle selector, available with 
  ISceneNode::getTriangleScelector() or ISceneManager::createTriangleSelector.
- This is used for doing collision detection: For example if you know, that a collision may happened in the 
- area between (1,1,1) and (10,10,10), you can get all triangles of the
- scene node in this area with the ITriangleSelector easily and check every triangle
- if it collided. */
+ This is used for doing collision detection: For example if you know, that a
+ collision may have happened in the area between (1,1,1) and (10,10,10), you
+ can get all triangles of the scene node in this area with the
+ ITriangleSelector easily and check every triangle if it collided. */
 class ITriangleSelector : public virtual IReferenceCounted
 {
 public:
@@ -34,44 +34,50 @@ public:
 	virtual s32 getTriangleCount() const = 0;
 
 	//! Gets all triangles.
-	//! \param triangles: Array where the resulting triangles will be written to.
+	//! \param triangles: Array where the resulting triangles will be
+	//! written to.
 	//! \param arraySize: Size of the target array.
-	//! \param outTriangleCount: Amount of triangles which have been written into the
-	//! array.
-	//! \param transform: Pointer to matrix for transforming the triangles before.
-	//! they are returned. Useful for example to scale all triangles down into an
-	//! ellipsoid space. If this pointer is null, no transformation will be done.
+	//! \param outTriangleCount: Amount of triangles which have been
+	//! written into the array.
+	//! \param transform: Pointer to matrix for transforming the triangles
+	//! before they are returned. Useful for example to scale all triangles
+	//! down into an ellipsoid space. If this pointer is null, no
+	//! transformation will be done.
 	virtual void getTriangles(core::triangle3df* triangles, s32 arraySize,
 		s32& outTriangleCount, const core::matrix4* transform=0) const = 0;
 
 	//! Gets all triangles which lie within a specific bounding box.
-	//! Please note that unoptimized triangle selectors also may return triangles,
-	//! which are not in the specific box at all.
-	//! \param triangles: Array where the resulting triangles will be written to.
+	//! Please note that unoptimized triangle selectors also may return
+	//! triangles which are not in the specific box at all.
+	//! \param triangles: Array where the resulting triangles will be
+	//! written to.
 	//! \param arraySize: Size of the target array.
-	//! \param outTriangleCount: Amount of triangles which have been written into the
-	//! array.
-	//! \param box: Only triangles which are in this axis aligned bounding box
-	//! will be written into the array.
-	//! \param transform: Pointer to matrix for transforming the triangles before.
-	//! they are returned. Useful for example to scale all triangles down into an
-	//! ellipsoid space. If this pointer is null, no transformation will be done.
+	//! \param outTriangleCount: Amount of triangles which have been
+	//! written into the array.
+	//! \param box: Only triangles which are in this axis aligned bounding
+	//! box will be written into the array.
+	//! \param transform: Pointer to matrix for transforming the triangles
+	//! before they are returned. Useful for example to scale all triangles
+	//! down into an ellipsoid space. If this pointer is null, no
+	//! transformation will be done.
 	virtual void getTriangles(core::triangle3df* triangles, s32 arraySize,
 		s32& outTriangleCount, const core::aabbox3d<f32>& box, 
 		const core::matrix4* transform=0) const = 0;
 
 	//! Gets all triangles which have or may have contact with a 3d line.
-	//! Please note that unoptimized triangle selectors also may return triangles,
-	//! which are not in contact at all with the 3d line.
-	//! \param triangles: Array where the resulting triangles will be written to.
+	//! Please note that unoptimized triangle selectors also may return
+	//! triangles which are not in contact at all with the 3d line.
+	//! \param triangles: Array where the resulting triangles will be
+	//! written to.
 	//! \param arraySize: Size of the target array.
-	//! \param outTriangleCount: Amount of triangles which have been written into the
-	//! array.
-	//! \param line: Only triangles which may be in contact with this 3d line will
-	//! be written into the array.
-	//! \param transform: Pointer to matrix for transforming the triangles before.
-	//! they are returned. Useful for example to scale all triangles down into an
-	//! ellipsoid space. If this pointer is null, no transformation will be done.
+	//! \param outTriangleCount: Amount of triangles which have been
+	//! written into the array.
+	//! \param line: Only triangles which may be in contact with this 3d
+	//! line will be written into the array.
+	//! \param transform: Pointer to matrix for transforming the triangles
+	//! before they are returned. Useful for example to scale all triangles
+	//! down into an ellipsoid space. If this pointer is null, no
+	//! transformation will be done.
 	virtual void getTriangles(core::triangle3df* triangles, s32 arraySize,
 		s32& outTriangleCount, const core::line3d<f32>& line, 
 		const core::matrix4* transform=0) const = 0;
