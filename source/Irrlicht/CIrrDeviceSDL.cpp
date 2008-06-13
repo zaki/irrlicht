4fd4e9e21f24dc7f9e1cf8306dfd5de8e3a0e189
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-06-13 09:39:30 +0000

    Added a new device creation parameter to choose alpha channel support for the color buffer.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@1372 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CIrrDeviceSDL.cpp
+++ source/Irrlicht/CIrrDeviceSDL.cpp
@@ -116,15 +116,17 @@ bool CIrrDeviceSDL::createWindow()
 	{
 		if (CreationParams.Bits==16)
 		{
-			SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
-			SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
-			SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
+			SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 4 );
+			SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 4 );
+			SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 4 );
+			SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, CreationParams.WithAlphaChannel?1:0 );
 		}
 		else
 		{
 			SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
 			SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
 			SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+			SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, CreationParams.WithAlphaChannel?8:0 );
 		}
 		SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, CreationParams.Bits);
 		SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
