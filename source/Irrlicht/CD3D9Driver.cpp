1edc55c9242d327af0ea53295c6a68f2988b4bbb
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-09-12 16:39:03 +0000

    Fixed a missing function call, found by night_hawk.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@1561 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CD3D9Driver.cpp
+++ source/Irrlicht/CD3D9Driver.cpp
@@ -1678,7 +1678,7 @@ void CD3D9Driver::setBasicRenderStates(const SMaterial& material, const SMateria
 	// zwrite
 //	if (resetAllRenderstates || (lastmaterial.ZWriteEnable != material.ZWriteEnable))
 	{
-		if ( material.ZWriteEnable && !(DisableZWriteOnTransparent && material.isTransparent))
+		if ( material.ZWriteEnable && !(DisableZWriteOnTransparent && material.isTransparent()))
 			pID3DDevice->SetRenderState( D3DRS_ZWRITEENABLE, TRUE);
 		else
 			pID3DDevice->SetRenderState( D3DRS_ZWRITEENABLE, FALSE);
