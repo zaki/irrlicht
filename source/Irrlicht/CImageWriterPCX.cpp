ae15b94a7fdd92774187e74c81cd6e7de2b1a408
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-10-29 22:07:19 +0000

    PCX Writer implementation, 24bit RLE, which means it's significantly smaller than BMP, but PNG and JPEG are still far smaller...
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@1660 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CImageWriterPCX.cpp
+++ source/Irrlicht/CImageWriterPCX.cpp
@@ -35,8 +35,103 @@ bool CImageWriterPCX::isAWriteableFileExtension(const c8* fileName) const
 
 bool CImageWriterPCX::writeImage(io::IWriteFile *file, IImage *image,u32 param) const
 {
-	os::Printer::log("PCX writer not yet implemented. Image not written.", ELL_WARNING);
-	return false;
+	if (!file || !image)
+		return false;
+
+	u8 d1;
+	u16 d2;
+	s32 i;
+
+	d1 = 10; // Manufacturer
+	file->write(&d1, 1);
+	d1 = 5; // Version
+	file->write(&d1, 1);
+	d1 = 1; // Encoding
+	file->write(&d1, 1);
+	d1 = 8; // Bits per Pixel
+	file->write(&d1, 1);
+	d2 = 0; // pixel origin
+	file->write(&d2, 2);
+	file->write(&d2, 2);
+	d2 = image->getDimension().Width-1; // width
+	file->write(&d2, 2);
+	d2 = image->getDimension().Height-1; // height
+	file->write(&d2, 2);
+	d2 = 300; // dpi
+	file->write(&d2, 2);
+	file->write(&d2, 2);
+	d2 = 0; // palette (not used)
+	for (i=0; i<24; ++i)
+	{
+		file->write(&d2, 2);
+	}
+	d1 = 0; // reserved
+	file->write(&d1, 1);
+	d1 = 3; // planes
+	file->write(&d1, 1);
+	d2 = image->getDimension().Width; // pitch
+	if (d2&0x0001) // must be even
+		++d2;
+	file->write(&d2, 2);
+	d2 = 1; // color mode
+	file->write(&d2, 2);
+	d2 = 800; // screen width
+	file->write(&d2, 2);
+	d2 = 600; // screen height
+	file->write(&d2, 2);
+	d2 = 0; // filler (not used)
+	for (i=0; i<27; ++i)
+	{
+		file->write(&d2, 2);
+	}
+
+	u8 cnt, value;
+	for (i=0; i<image->getDimension().Height; ++i)
+	{
+		cnt = 0;
+		value = 0;
+		for (u32 j=0; j<3; ++j) // color planes
+		{
+			for (s32 k=0; k<image->getDimension().Width; ++k)
+			{
+				const SColor pix = image->getPixel(k,i);
+				if ((cnt!=0) && (cnt<63) &&
+					(((j==0) && (value==pix.getRed())) ||
+					((j==1) && (value==pix.getGreen())) ||
+					((j==2) && (value==pix.getBlue()))))
+				{
+					++cnt;
+				}
+				else
+				{
+					if (cnt!=0)
+					{
+						if ((cnt>1) || ((value&0xc0)==0xc0))
+						{
+							cnt |= 0xc0;
+							file->write(&cnt, 1);
+						}
+						file->write(&value, 1);
+					}
+					cnt=1;
+					if (j==0)
+						value=pix.getRed();
+					else if (j==1)
+						value=pix.getGreen();
+					else if (j==2)
+						value=pix.getBlue();
+				}
+			}
+		}
+		if ((cnt>1) || ((value&0xc0)==0xc0))
+		{
+			cnt |= 0xc0;
+			file->write(&cnt, 1);
+		}
+		file->write(&value, 1);
+	}
+
+	return true;
 }
 
 } // namespace video
