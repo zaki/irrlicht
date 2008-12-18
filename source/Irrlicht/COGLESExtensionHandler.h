62d85c87587a566497434639163ba0107913404c
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-12-18 00:10:18 +0000

    Added separate extension handler file with some new functions. Moved code from the header file there.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/ogl-es@1972 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/COGLESExtensionHandler.h
+++ source/Irrlicht/COGLESExtensionHandler.h
@@ -9,10 +9,11 @@
 #include "IrrCompileConfig.h"
 
 #ifdef _IRR_COMPILE_WITH_OGLES1_
-
 #include <GLES/egl.h>
 #include <GLES/gl.h>
+#include "gles-ext.h"
 #include "os.h"
+#include "EDriverFeatures.h"
 
 #ifndef GL_BGRA
 // we need to do this for the IMG_BGRA8888 extension
@@ -23,51 +24,6 @@ namespace irr
 {
 namespace video
 {
-static const char* const OGLESFeatureStrings[] = {
-	"GL_AMD_compressed_3DC_texture",
-	"GL_AMD_compressed_ATC_texture",
-	"GL_ARB_texture_env_combine",
-	"GL_ARB_texture_env_dot3",
-	"GL_EXT_multi_draw_arrays",
-	"GL_EXT_texture_filter_anisotropic",
-	"GL_IMG_read_format",
-	"GL_IMG_texture_compression_pvrtc",
-	"GL_IMG_texture_env_enhanced_fixed_function",
-	"GL_IMG_texture_format_BGRA8888",
-	"GL_IMG_user_clip_planes",
-	"GL_IMG_vertex_program",
-	"GL_OES_blend_equation_separate",
-	"GL_OES_blend_func_separate",
-	"GL_OES_blend_subtract",
-	"GL_OES_byte_coordinates",
-	"GL_OES_compressed_ETC1_RGB8_texture",
-	"GL_OES_compressed_paletted_texture",
-	"GL_OES_depth24",
-	"GL_OES_depth32",
-	"GL_OES_draw_texture",
-	"GL_OES_EGL_image",
-	"GL_OES_element_index_uint",
-	"GL_OES_extended_matrix_palette",
-	"GL_OES_fbo_render_mipmap",
-	"GL_OES_fixed_point",
-	"GL_OES_framebuffer_object",
-	"GL_OES_mapbuffer",
-	"GL_OES_matrix_get",
-	"GL_OES_matrix_palette",
-	"GL_OES_point_size_array",
-	"GL_OES_point_sprite",
-	"GL_OES_query_matrix",
-	"GL_OES_read_format",
-	"GL_OES_rgb8_rgba8",
-	"GL_OES_single_precision",
-	"GL_OES_stencil1",
-	"GL_OES_stencil4",
-	"GL_OES_stencil8",
-	"GL_OES_stencil_wrap",
-	"GL_OES_texture_cube_map",
-	"GL_OES_texture_env_crossbar",
-	"GL_OES_texture_mirrored_repeat"
-};
 
 	class COGLES1ExtensionHandler
 	{
@@ -120,14 +76,7 @@ static const char* const OGLESFeatureStrings[] = {
 		IRR_OGLES_Feature_Count
 	};
 
-		COGLES1ExtensionHandler() : Version(0), MaxUserClipPlanes(0),
-			MaxTextureUnits(0), MaxLights(0),
-			MultiTextureExtension(false), MultiSamplingExtension(false),
-			StencilBuffer(false)
-		{
-			for (u32 i=0; i<IRR_OGLES_Feature_Count; ++i)
-				FeatureAvailable[i]=false;
-		}
+		COGLES1ExtensionHandler();
 
 		bool queryFeature(video::E_VIDEO_DRIVER_FEATURE feature) const
 		{
@@ -152,64 +101,59 @@ static const char* const OGLESFeatureStrings[] = {
 			}
 		}
 
-		void dump() const
-		{
-			for (u32 i=0; i<IRR_OGLES_Feature_Count; ++i)
-				os::Printer::log(OGLESFeatureStrings[i], FeatureAvailable[i]?" true":" false");
-		}
+		void dump() const;
 
-		void initExtensions(EGLDisplay display, bool withStencil)
-		{
-			const f32 ogl_ver = core::fast_atof(reinterpret_cast<const c8*>(eglQueryString(display, EGL_VERSION)));
-			Version = core::floor32(ogl_ver)*100+core::ceil32(core::fract(ogl_ver)*10.0f);
-			core::stringc eglExtensions = eglQueryString(display, EGL_EXTENSIONS);
-			core::stringc extensions = glGetString(GL_EXTENSIONS);
-			os::Printer::log(extensions.c_str());
-			{
-				c8* str = new c8[extensions.size()+1];
-				strncpy(str, extensions.c_str(), extensions.size()+1);
-				c8* p = str;
-
-				for (u32 i=0; i<extensions.size(); ++i)
-				{
-					if (str[i] == ' ')
-					{
-						str[i] = 0;
-						if (*p)
-						for (u32 j=0; j<IRR_OGLES_Feature_Count; ++j)
-						{
-							if (!strcmp(OGLESFeatureStrings[j], p))
-							{
-								FeatureAvailable[j] = true;
-								break;
-							}
-						}
-
-						p = p + strlen(p) + 1;
-					}
-				}
-
-				delete [] str;
-			}
-#ifndef GL_BGRA
-			// whoa, pretty badly implemented extension...
-			if (FeatureAvailable[IRR_IMG_texture_format_BGRA8888])
-				GL_BGRA=0x80E1;
-			else
-				GL_BGRA=GL_RGBA;
-#endif
-
-			GLint val=0;
-//			glGetIntegerv(GL_MAX_TEXTURES, &val);
-			MaxTextureUnits = 2;
-			MultiTextureExtension = true;
-			glGetIntegerv(GL_MAX_CLIP_PLANES, &val);
-			MaxUserClipPlanes=val;
-			glGetIntegerv(GL_MAX_LIGHTS, &val);
-			MaxLights = val;
-		}
+		void initExtensions(EGLDisplay display, bool withStencil);
 
 	public:
+		void extGlDrawTex(GLfloat X, GLfloat Y, GLfloat Z, GLfloat W, GLfloat H)
+		{
+#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
+			if (pGlDrawTexfOES)
+				pGlDrawTexfOES(X, Y, Z, W, H);
+#elif defined(GL_OES_draw_texture)
+			glDrawTexfOES(X, Y, Z, W, H);
+#else
+			os::Printer::log("glDrawTexture not supported", ELL_ERROR);
+#endif
+		}
+
+		void extGlDrawTex(GLint X, GLint Y, GLint Z, GLint W, GLint H)
+		{
+#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
+			if (pGlDrawTexiOES)
+				pGlDrawTexiOES(X, Y, Z, W, H);
+#elif defined(GL_OES_draw_texture)
+			glDrawTexiOES(X, Y, Z, W, H);
+#else
+			os::Printer::log("glDrawTexture not supported", ELL_ERROR);
+#endif
+		}
+
+		void extGlDrawTex(GLfloat* coords)
+		{
+#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
+			if (pGlDrawTexfvOES)
+				pGlDrawTexfvOES(coords);
+#elif defined(GL_OES_draw_texture)
+			glDrawTexfvOES(coords);
+#else
+			os::Printer::log("glDrawTexture not supported", ELL_ERROR);
+#endif
+		}
+
+		void extGlDrawTex(GLint* coords)
+		{
+#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
+			if (pGlDrawTexivOES)
+				pGlDrawTexivOES(coords);
+#elif defined(GL_OES_draw_texture)
+			glDrawTexivOES(coords);
+#else
+			os::Printer::log("glDrawTexture not supported", ELL_ERROR);
+#endif
+		}
+
 		// we need to implement some methods which have been extensions in the original OpenGL driver
 		void extGlActiveTexture(GLenum texture)
 		{
@@ -248,6 +192,18 @@ static const char* const OGLESFeatureStrings[] = {
 			glPointParameterfv(loc, v);
 		}
 
+	private:
+#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
+		typedef void (APIENTRY * PFNGLDRAWTEXIOES) (GLint x, GLint y, GLint z, GLint width, GLint height);
+		typedef void (APIENTRY * PFNGLDRAWTEXIVOES) (const GLint* coords);
+		typedef void (APIENTRY * PFNGLDRAWTEXFOES) (GLfloat x, GLfloat y, GLfloat z, GLfloat width, GLfloat height);
+		typedef void (APIENTRY * PFNGLDRAWTEXFVOES) (const GLfloat* coords);
+		PFNGLDRAWTEXIOES pGlDrawTexiOES;
+		PFNGLDRAWTEXFOES pGlDrawTexfOES;
+		PFNGLDRAWTEXIVOES pGlDrawTexivOES;
+		PFNGLDRAWTEXFVOES pGlDrawTexfvOES;
+#endif
+
 	protected:
 		bool FeatureAvailable[IRR_OGLES_Feature_Count];
 		s32 Version;
@@ -265,6 +221,3 @@ static const char* const OGLESFeatureStrings[] = {
 
 #endif // _IRR_COMPILE_WITH_OPENGL_
 #endif
-
-
-
