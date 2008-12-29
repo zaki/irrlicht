c9d7d28b90a9e3f4d25e83cc7d9987cabca1cf37
rogerborg <rogerborg@dfc29bdd-3216-0410-991c-e03cc46cb475>
2008-12-29 16:31:18 +0000

    Add missing irrCoreEquals.cpp test file.  Update changes.txt to note that position2d is now a synonym for vector2d.  Add proper doxygen \deprecated tags for position2d.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@2001 dfc29bdd-3216-0410-991c-e03cc46cb475

---
+++ tests/irrCoreEquals.cpp
@@ -0,0 +1,84 @@
+// Copyright (C) 2008 Colin MacDonald
+// No rights reserved: this software is in the public domain.
+
+#include "testUtils.h"
+#include "irrlicht.h"
+#include <assert.h>
+
+bool irrCoreEquals(void)
+{
+	if(!irr::core::equals(99.f, 99.f))
+	{
+		logTestString("irr::core::equals(f32, f32 (, default)) failed.\n");
+		return false;
+	}
+
+	if(!irr::core::equals(99.f, 98.f, 1.f))
+	{
+		logTestString("irr::core::equals(f32, f32, f32) failed.\n");
+		return false;
+	}
+
+	if(!irr::core::equals(99.0, 99.0))
+	{
+		logTestString("irr::core::equals(f64, f64 (,default)) failed.\n");
+		return false;
+	}
+
+	if(!irr::core::equals(99.0, 98.0, 1.0))
+	{
+		logTestString("irr::core::equals(f64, f64, f64) failed.\n");
+		return false;
+	}
+
+	if(!irr::core::equals(99, 99))
+	{
+		logTestString("irr::core::equals(s32, s32 (,default)) failed.\n");
+		return false;
+	}
+
+	if(!irr::core::equals(99, 98, 1))
+	{
+		logTestString("irr::core::equals(s32, s32, s32) failed.\n");
+		return false;
+	}
+
+	if(irr::core::equals(99, 98))
+	{
+		logTestString("irr::core::equals(s32, s32 (,default)) failed.\n");
+		return false;
+	}
+
+	if(irr::core::equals(99, 98, 0))
+	{
+		logTestString("irr::core::equals(s32, s32, 0) failed.\n");
+		return false;
+	}
+
+	if(!irr::core::equals(-99, -99))
+	{
+		logTestString("irr::core::equals(s32, s32 (,default)) failed.\n");
+		return false;
+	}
+
+	if(!irr::core::equals(-99, -98, 1))
+	{
+		logTestString("irr::core::equals(s32, s32, s32) failed.\n");
+		return false;
+	}
+
+	if(irr::core::equals(-99, -98))
+	{
+		logTestString("irr::core::equals(s32, s32 (,default)) failed.\n");
+		return false;
+	}
+
+	if(irr::core::equals(-99, -98, 0))
+	{
+		logTestString("irr::core::equals(s32, s32, 0) failed.\n");
+		return false;
+	}
+
+
+	return true;
+}
