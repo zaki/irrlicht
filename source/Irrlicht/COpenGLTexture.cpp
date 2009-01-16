90eec5ade9e4dbd0acb3ebff56edda7e76a8798f
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-01-16 08:50:25 +0000

    Fix hint name.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@2080 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/COpenGLTexture.cpp
+++ source/Irrlicht/COpenGLTexture.cpp
@@ -189,11 +189,11 @@ void COpenGLTexture::copyTexture(bool newTexture)
 		if (HasMipMaps && Driver->queryFeature(EVDF_MIP_MAP_AUTO_UPDATE))
 		{
 			if (Driver->getTextureCreationFlag(ETCF_OPTIMIZED_FOR_SPEED))
-				glHint(GENERATE_MIPMAP_HINT_SGIS, GL_FASTEST);
+				glHint(GL_GENERATE_MIPMAP_HINT_SGIS, GL_FASTEST);
 			else if (Driver->getTextureCreationFlag(ETCF_OPTIMIZED_FOR_QUALITY))
-				glHint(GENERATE_MIPMAP_HINT_SGIS, GL_NICEST);
+				glHint(GL_GENERATE_MIPMAP_HINT_SGIS, GL_NICEST);
 			else
-				glHint(GENERATE_MIPMAP_HINT_SGIS, GL_DONT_CARE);
+				glHint(GL_GENERATE_MIPMAP_HINT_SGIS, GL_DONT_CARE);
 			// automatically generate and update mipmaps
 			glTexParameteri( GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE );
 			AutomaticMipmapUpdate=true;
