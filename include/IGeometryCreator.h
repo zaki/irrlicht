df3059729421f2e51f7b8bff3a9278249dfa8637
Rogerborg <Rogerborg@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-02-10 13:40:13 +0000

    Add missing IGeometryCreator.h file.  It's missing comments (which I'll add tonight) along with tests/testGeometryCreate.cpp.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@2206 dfc29bdd-3216-0410-991c-e03cc46cb475

---
+++ include/IGeometryCreator.h
@@ -0,0 +1,55 @@
+// Copyright (C) 2002-2009 Nikolaus Gebhardt
+// This file is part of the "Irrlicht Engine".
+// For conditions of distribution and use, see copyright notice in irrlicht.h
+
+#ifndef __I_GEOMETRY_CREATOR_H_INCLUDED__
+#define __I_GEOMETRY_CREATOR_H_INCLUDED__
+
+#include "IMesh.h"
+#include "IVideoDriver.h"
+
+namespace irr
+{
+
+namespace scene
+{
+
+//! class for creating geometry on the fly
+class IGeometryCreator
+{
+public:
+
+	virtual IMesh* createHillPlaneMesh(
+		const core::dimension2d<f32>& tileSize, const core::dimension2d<u32>& tileCount,
+		video::SMaterial* material, f32 hillHeight, const core::dimension2d<f32>& countHills,
+		const core::dimension2d<f32>& textureRepeatCount) const = 0;
+
+	virtual IMesh* createTerrainMesh(video::IImage* texture,
+		video::IImage* heightmap, const core::dimension2d<f32>& stretchSize,
+		f32 maxHeight, video::IVideoDriver* driver,
+		const core::dimension2d<u32>& defaultVertexBlockSize,
+		bool debugBorders=false) const = 0;
+
+	virtual IMesh* createArrowMesh(const u32 tesselationCylinder,
+			const u32 tesselationCone, const f32 height,
+			const f32 cylinderHeight, const f32 width0,
+			const f32 width1, const video::SColor vtxColor0,
+			const video::SColor vtxColor1) const = 0;
+
+	virtual IMesh* createSphereMesh(f32 radius, u32 polyCountX, u32 polyCountY) const = 0;
+
+	virtual IMesh* createCylinderMesh(f32 radius, f32 length, u32 tesselation,
+							const video::SColor& color=video::SColor(0xffffffff),
+							bool closeTop=true, f32 oblique=0.f) const = 0;
+
+	virtual IMesh* createConeMesh(f32 radius, f32 length, u32 tesselation,
+						const video::SColor& colorTop=video::SColor(0xffffffff),
+						const video::SColor& colorBottom=video::SColor(0xffffffff), f32 oblique=0.f) const = 0;
+};
+
+
+} // end namespace scene
+} // end namespace irr
+
+#endif
+
