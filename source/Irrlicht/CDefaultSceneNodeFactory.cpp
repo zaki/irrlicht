6e1fc8d745323deed24aa37f48f36ca5233c80f7
bitplane <bitplane@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-03-05 18:20:07 +0000

    Add sky dome type and to default node factory, add serialization and clone methods (bug #2656990 reported by lab_zj)
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@2254 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CDefaultSceneNodeFactory.cpp
+++ source/Irrlicht/CDefaultSceneNodeFactory.cpp
@@ -36,6 +36,7 @@ CDefaultSceneNodeFactory::CDefaultSceneNodeFactory(ISceneManager* mgr)
 	SupportedSceneNodeTypes.push_back(SSceneNodeTypePair(ESNT_WATER_SURFACE, "waterSurface"));
 	SupportedSceneNodeTypes.push_back(SSceneNodeTypePair(ESNT_TERRAIN, "terrain"));
 	SupportedSceneNodeTypes.push_back(SSceneNodeTypePair(ESNT_SKY_BOX, "skyBox"));
+	SupportedSceneNodeTypes.push_back(SSceneNodeTypePair(ESNT_SKY_DOME, "skyDome"));
 	SupportedSceneNodeTypes.push_back(SSceneNodeTypePair(ESNT_SHADOW_VOLUME, "shadowVolume"));
 	SupportedSceneNodeTypes.push_back(SSceneNodeTypePair(ESNT_OCT_TREE, "octTree"));
 	SupportedSceneNodeTypes.push_back(SSceneNodeTypePair(ESNT_MESH, "mesh"));
@@ -77,6 +78,8 @@ ISceneNode* CDefaultSceneNodeFactory::addSceneNode(ESCENE_NODE_TYPE type, IScene
 							4, ETPS_17, 0, true);
 	case ESNT_SKY_BOX:
 		return Manager->addSkyBoxSceneNode(0,0,0,0,0,0, parent);
+	case ESNT_SKY_DOME:
+		return Manager->addSkyDomeSceneNode(0, 16, 8, 0.9f, 2.0f, 1000.0f, parent);
 	case ESNT_SHADOW_VOLUME:
 		return 0; 
 	case ESNT_OCT_TREE:
