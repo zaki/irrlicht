2d74d5d39531f2de5aa511df7086ab7499426313
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-06-09 00:53:05 +0000

    New test for zipreader (works).
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@2414 dfc29bdd-3216-0410-991c-e03cc46cb475

--- tests/main.cpp
+++ tests/main.cpp
@@ -57,6 +57,7 @@ int main(int argumentCount, char * arguments[])
 	// process.
 
 	TEST(disambiguateTextures); // Normally you should run this first, since it validates the working directory.
+	TEST(zipReader);
 	TEST(exports);
 	TEST(sceneCollisionManager);
 	TEST(testVector3d);
