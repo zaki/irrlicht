997d920b1ff1e124343f8d65843770b6119c280c
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-08-03 12:18:47 +0000

    Add support for scaling button images to fit.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@2548 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CGUIButton.h
+++ source/Irrlicht/CGUIButton.h
@@ -38,19 +38,19 @@ namespace gui
 		virtual void setOverrideFont(IGUIFont* font=0);
 
 		//! Sets an image which should be displayed on the button when it is in normal state.
-		virtual void setImage(video::ITexture* image);
+		virtual void setImage(video::ITexture* image=0);
 
 		//! Sets an image which should be displayed on the button when it is in normal state.
 		virtual void setImage(video::ITexture* image, const core::rect<s32>& pos);
 
 		//! Sets an image which should be displayed on the button when it is in pressed state.
-		virtual void setPressedImage(video::ITexture* image);
+		virtual void setPressedImage(video::ITexture* image=0);
 
 		//! Sets an image which should be displayed on the button when it is in pressed state.
 		virtual void setPressedImage(video::ITexture* image, const core::rect<s32>& pos);
 
 		//! Sets the sprite bank used by the button
-		virtual void setSpriteBank(IGUISpriteBank* bank);
+		virtual void setSpriteBank(IGUISpriteBank* bank=0);
 
 		//! Sets the animated sprite for a specific button state
 		/** \param index: Number of the sprite within the sprite bank, use -1 for no sprite
@@ -64,28 +64,34 @@ namespace gui
 		//! Sets if the button should behave like a push button. Which means it
 		//! can be in two states: Normal or Pressed. With a click on the button,
 		//! the user can change the state of the button.
-		virtual void setIsPushButton(bool isPushButton);
+		virtual void setIsPushButton(bool isPushButton=true);
+
+		//! Checks whether the button is a push button
+		virtual bool isPushButton() const;
+
+		//! Sets the pressed state of the button if this is a pushbutton
+		virtual void setPressed(bool pressed=true);
 
 		//! Returns if the button is currently pressed
 		virtual bool isPressed() const;
 
-		//! Sets the pressed state of the button if this is a pushbutton
-		virtual void setPressed(bool pressed);
-
 		//! Sets if the button should use the skin to draw its border
-		virtual void setDrawBorder(bool border);
+		virtual void setDrawBorder(bool border=true);
 
-		//! Sets if the alpha channel should be used for drawing images on the button (default is false)
-		virtual void setUseAlphaChannel(bool useAlphaChannel);
-
-		//! Returns if the alpha channel should be used for drawing images on the button
-		virtual bool isAlphaChannelUsed() const;
-
-		//! Returns if the button face and border are being drawn
+		//! Checks if the button face and border are being drawn
 		virtual bool isDrawingBorder() const;
 
-		//! Returns whether the button is a push button
-		virtual bool isPushButton() const;
+		//! Sets if the alpha channel should be used for drawing images on the button (default is false)
+		virtual void setUseAlphaChannel(bool useAlphaChannel=true);
+
+		//! Checks if the alpha channel should be used for drawing images on the button
+		virtual bool isAlphaChannelUsed() const;
+
+		//! Sets if the button should scale the button images to fit
+		virtual void setScaleImage(bool scaleImage=true);
+
+		//! Checks whether the button scales the used images
+		virtual bool isScalingImage() const;
 
 		//! Writes attributes of the element.
 		virtual void serializeAttributes(io::IAttributes* out, io::SAttributeReadWriteOptions* options) const;
@@ -102,23 +108,24 @@ namespace gui
 			bool Loop;
 		};
 
-		bool Pressed;
-		bool IsPushButton;
-		bool UseAlphaChannel;
-		bool Border;
-
-		u32 ClickTime;
+		ButtonSprite ButtonSprites[EGBS_COUNT];
 
 		IGUISpriteBank* SpriteBank;
 		IGUIFont* OverrideFont;
-
-		ButtonSprite ButtonSprites[EGBS_COUNT];
 
 		video::ITexture* Image;
 		video::ITexture* PressedImage;
 
 		core::rect<s32> ImageRect;
 		core::rect<s32> PressedImageRect;
+
+		u32 ClickTime;
+
+		bool IsPushButton;
+		bool Pressed;
+		bool UseAlphaChannel;
+		bool DrawBorder;
+		bool ScaleImage;
 	};
 
 } // end namespace gui
