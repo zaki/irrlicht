95ed92d8792a6374de663a02013763be93c0561b
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-11-10 16:30:34 +0000

    Merged revisions 2778:2832 from 1.5 branch. Fix a driver feature return value and a render state bug for OpenGL.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@2833 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CSceneManager.cpp
+++ source/Irrlicht/CSceneManager.cpp
@@ -1326,6 +1326,7 @@ void CSceneManager::drawAll()
 	video::IVideoDriver* driver = getVideoDriver();
 	if ( driver )
 	{
+		driver->setMaterial(video::SMaterial());
 		driver->setTransform ( video::ETS_PROJECTION, core::IdentityMatrix );
 		driver->setTransform ( video::ETS_VIEW, core::IdentityMatrix );
 		driver->setTransform ( video::ETS_WORLD, core::IdentityMatrix );
@@ -1914,6 +1915,9 @@ void CSceneManager::removeAll()
 {
 	ISceneNode::removeAll();
 	setActiveCamera(0);
+	// Make sure the driver is reset, might need a more complex method at some point
+	if (Driver)
+		Driver->setMaterial(video::SMaterial());
 }
 
 
