f3bf8b1f2b5b18cac648f459dfb1b69438986a86
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-11-13 13:46:12 +0000

    Fix createMeshWith1TCoords to avoid vertex duplication. Add a new VertexManipulator interface, which allows to easily add manipulators. Several existing methods have been replaced by this interface. Further manipulation schemes will be added later.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@2856 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IMeshManipulator.h
+++ include/IMeshManipulator.h
@@ -10,7 +10,7 @@
 #include "aabbox3d.h"
 #include "matrix4.h"
 #include "IAnimatedMesh.h"
-#include "SColor.h"
+#include "SVertexManipulator.h"
 
 namespace irr
 {
@@ -40,12 +40,18 @@ namespace scene
 		//! Sets the alpha vertex color value of the whole mesh to a new value.
 		/** \param mesh Mesh on which the operation is performed.
 		\param alpha New alpha value. Must be a value between 0 and 255. */
-		virtual void setVertexColorAlpha(IMesh* mesh, s32 alpha) const = 0;
+		void setVertexColorAlpha(IMesh* mesh, s32 alpha) const
+		{
+			apply(scene::SVertexColorSetAlphaManipulator(alpha), mesh);
+		}
 
 		//! Sets the colors of all vertices to one color
 		/** \param mesh Mesh on which the operation is performed.
 		\param color New color. */
-		virtual void setVertexColors(IMesh* mesh, video::SColor color) const = 0;
+		void setVertexColors(IMesh* mesh, video::SColor color) const
+		{
+			apply(scene::SVertexColorSetManipulator(color), mesh);
+		}
 
 		//! Recalculates all normals of the mesh.
 		/** \param mesh: Mesh on which the operation is performed.
@@ -62,40 +68,58 @@ namespace scene
 		//! Scales the actual mesh, not a scene node.
 		/** \param mesh Mesh on which the operation is performed.
 		\param factor Scale factor for each axis. */
-		virtual void scale(IMesh* mesh, const core::vector3df& factor) const = 0;
+		void scale(IMesh* mesh, const core::vector3df& factor) const
+		{
+			apply(SVertexPositionScaleManipulator(factor), mesh, true);
+		}
 
 		//! Scales the actual meshbuffer, not a scene node.
 		/** \param buffer Meshbuffer on which the operation is performed.
 		\param factor Scale factor for each axis. */
-		virtual void scale(IMeshBuffer* buffer, const core::vector3df& factor) const = 0;
+		void scale(IMeshBuffer* buffer, const core::vector3df& factor) const
+		{
+			apply(SVertexPositionScaleManipulator(factor), buffer, true);
+		}
 
 		//! Scales the actual mesh, not a scene node.
 		/** \deprecated Use scale() instead
 		\param mesh Mesh on which the operation is performed.
 		\param factor Scale factor for each axis. */
-		virtual void scaleMesh(IMesh* mesh, const core::vector3df& factor) const {return scale(mesh,factor);}
+		void scaleMesh(IMesh* mesh, const core::vector3df& factor) const {return scale(mesh,factor);}
 
 		//! Scale the texture coords of a mesh.
 		/** \param mesh Mesh on which the operation is performed.
 		\param factor Vector which defines the scale for each axis.
 		\param level Number of texture coord, starting from 1. Support for level 2 exists for LightMap buffers. */
-		virtual void scaleTCoords(scene::IMesh* mesh, const core::vector2df& factor, u32 level=1) const =0;
+		void scaleTCoords(scene::IMesh* mesh, const core::vector2df& factor, u32 level=1) const
+		{
+			apply(SVertexTCoordsScaleManipulator(factor, level), mesh);
+		}
 
 		//! Scale the texture coords of a meshbuffer.
 		/** \param buffer Meshbuffer on which the operation is performed.
 		\param factor Vector which defines the scale for each axis.
 		\param level Number of texture coord, starting from 1. Support for level 2 exists for LightMap buffers. */
-		virtual void scaleTCoords(scene::IMeshBuffer* buffer, const core::vector2df& factor, u32 level=1) const =0;
+		void scaleTCoords(scene::IMeshBuffer* buffer, const core::vector2df& factor, u32 level=1) const
+		{
+			apply(SVertexTCoordsScaleManipulator(factor, level), buffer);
+		}
 
 		//! Applies a transformation to a mesh
 		/** \param mesh Mesh on which the operation is performed.
 		\param m transformation matrix. */
-		virtual void transform(IMesh* mesh, const core::matrix4& m) const = 0;
+		void transform(IMesh* mesh, const core::matrix4& m) const
+		{
+			apply(SVertexPositionTransformManipulator(m), mesh, true);
+		}
 
 		//! Applies a transformation to a meshbuffer
 		/** \param buffer Meshbuffer on which the operation is performed.
 		\param m transformation matrix. */
-		virtual void transform(IMeshBuffer* buffer, const core::matrix4& m) const = 0;
+		void transform(IMeshBuffer* buffer, const core::matrix4& m) const
+		{
+			apply(SVertexPositionTransformManipulator(m), buffer, true);
+		}
 
 		//! Applies a transformation to a mesh
 		/** \deprecated Use transform() instead
@@ -205,11 +229,100 @@ namespace scene
 		IReferenceCounted::drop() for more information. */
 		virtual IAnimatedMesh * createAnimatedMesh(IMesh* mesh,
 			scene::E_ANIMATED_MESH_TYPE type = scene::EAMT_UNKNOWN) const = 0;
-	};
+
+		//! Apply a manipulator on the Meshbuffer
+		/** \param func A functor defining the mesh manipulation.
+		\param buffer The Meshbuffer to apply the manipulator to.
+		\param boundingBoxUpdate Specifies if the bounding box should be updated during manipulation.
+		\return True if the functor was successfully applied, else false. */
+		template <typename Functor>
+		bool apply(const Functor& func, IMeshBuffer* buffer, bool boundingBoxUpdate=false) const
+		{
+			return apply_(func, buffer, boundingBoxUpdate, func);
+		}
+
+
+		//! Apply a manipulator on the Mesh
+		/** \param func A functor defining the mesh manipulation.
+		\param mesh The Mesh to apply the manipulator to.
+		\param boundingBoxUpdate Specifies if the bounding box should be updated during manipulation.
+		\return True if the functor was successfully applied, else false. */
+		template <typename Functor>
+		bool apply(const Functor& func, IMesh* mesh, bool boundingBoxUpdate=false) const
+		{
+			if (!mesh)
+				return true;
+			bool result = true;
+			core::aabbox3df bufferbox;
+			for (u32 i=0; i<mesh->getMeshBufferCount(); ++i)
+			{
+				result &= apply(func, mesh->getMeshBuffer(i), boundingBoxUpdate);
+				if (boundingBoxUpdate)
+				{
+					if (0==i)
+						bufferbox.reset(mesh->getBoundingBox());
+					else
+						bufferbox.addInternalBox(mesh->getBoundingBox());
+				}
+			}
+			if (boundingBoxUpdate)
+				mesh->setBoundingBox(bufferbox);
+			return result;
+		}
+
+protected:
+		//! Apply a manipulator based on the type of the functor
+		/** \param func A functor defining the mesh manipulation.
+		\param buffer The Meshbuffer to apply the manipulator to.
+		\param boundingBoxUpdate Specifies if the bounding box should be updated during manipulation.
+		\param typeTest Unused parameter, which handles the proper call selection based on the type of the Functor which is passed in two times.
+		\return True if the functor was successfully applied, else false. */
+		template <typename Functor>
+		bool apply_(const Functor& func, IMeshBuffer* buffer, bool boundingBoxUpdate, const IVertexManipulator& typeTest) const
+		{
+			if (!buffer)
+				return true;
+
+			core::aabbox3df bufferbox;
+			for (u32 i=0; i<buffer->getVertexCount(); ++i)
+			{
+				switch (buffer->getVertexType())
+				{
+				case video::EVT_STANDARD:
+					{
+						video::S3DVertex* verts = (video::S3DVertex*)buffer->getVertices();
+						func(verts[i]);
+					}
+					break;
+				case video::EVT_2TCOORDS:
+					{
+						video::S3DVertex2TCoords* verts = (video::S3DVertex2TCoords*)buffer->getVertices();
+						func(verts[i]);
+					}
+					break;
+				case video::EVT_TANGENTS:
+					{
+						video::S3DVertexTangents* verts = (video::S3DVertexTangents*)buffer->getVertices();
+						func(verts[i]);
+					}
+					break;
+				}
+				if (boundingBoxUpdate)
+				{
+					if (0==i)
+						bufferbox.reset(buffer->getPosition(0));
+					else
+						bufferbox.addInternalPoint(buffer->getPosition(i));
+				}
+			}
+			if (boundingBoxUpdate)
+				buffer->setBoundingBox(bufferbox);
+			return true;
+		}
+};
 
 } // end namespace scene
 } // end namespace irr
 
 
 #endif
-
