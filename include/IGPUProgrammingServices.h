ce55f4bf5216247626ff6347c81614a2b10cc974
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-11-24 16:31:50 +0000

    Split shader types definitions.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@2923 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IGPUProgrammingServices.h
+++ include/IGPUProgrammingServices.h
@@ -5,7 +5,8 @@
 #ifndef __I_GPU_PROGRAMMING_SERVICES_H_INCLUDED__
 #define __I_GPU_PROGRAMMING_SERVICES_H_INCLUDED__
 
-#include "SMaterial.h"
+#include "EShaderTypes.h"
+#include "EMaterialTypes.h"
 #include "path.h"
 
 namespace irr
@@ -21,54 +22,6 @@ namespace video
 
 class IVideoDriver;
 class IShaderConstantSetCallBack;
-
-//! Compile target enumeration for the addHighLevelShaderMaterial() method.
-enum E_VERTEX_SHADER_TYPE
-{
-	EVST_VS_1_1 = 0,
-	EVST_VS_2_0,
-	EVST_VS_2_a,
-	EVST_VS_3_0,
-
-	//! This is not a type, but a value indicating how much types there are.
-	EVST_COUNT
-};
-
-//! Names for all vertex shader types, each entry corresponds to a E_VERTEX_SHADER_TYPE entry.
-const c8* const VERTEX_SHADER_TYPE_NAMES[] = {
-	"vs_1_1",
-	"vs_2_0",
-	"vs_2_a",
-	"vs_3_0",
-	0 };
-
-//! Compile target enumeration for the addHighLevelShaderMaterial() method.
-enum E_PIXEL_SHADER_TYPE
-{
-	EPST_PS_1_1 = 0,
-	EPST_PS_1_2,
-	EPST_PS_1_3,
-	EPST_PS_1_4,
-	EPST_PS_2_0,
-	EPST_PS_2_a,
-	EPST_PS_2_b,
-	EPST_PS_3_0,
-
-	//! This is not a type, but a value indicating how much types there are.
-	EPST_COUNT
-};
-
-//! Names for all pixel shader types, each entry corresponds to a E_PIXEL_SHADER_TYPE entry.
-const c8* const PIXEL_SHADER_TYPE_NAMES[] = {
-	"ps_1_1",
-	"ps_1_2",
-	"ps_1_3",
-	"ps_1_4",
-	"ps_2_0",
-	"ps_2_a",
-	"ps_2_b",
-	"ps_3_0",
-	0 };
 
 //! Interface making it possible to create and use programs running on the GPU.
 class IGPUProgrammingServices
