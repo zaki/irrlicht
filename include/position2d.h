10e1bbab4df197949b0b37e3c0a825ac12e9f6aa
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-11-29 23:37:04 +0000

    Remove some extra ; because otherwise tests won't even compile.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@2944 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/position2d.h
+++ include/position2d.h
@@ -22,8 +22,8 @@ typedef vector2d<f32> position2df;
 
 //! \deprecated position2d is now a synonym for vector2d, but vector2d should be used directly.
 typedef vector2d<s32> position2di;
-}; // namespace core
-}; // namespace irr
+} // namespace core
+} // namespace irr
 
 // ...and use a #define to catch the rest, for (e.g.) position2d<f64>
 #define position2d vector2d
