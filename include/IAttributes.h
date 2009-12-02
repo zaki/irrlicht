3c00a1ffe330bbe6afa52dd2e3e44abd9d4ee387
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-12-02 16:02:41 +0000

    - CStringWArrayAttribute can use const-ref instead of full copy - Put E_ATTRIBUTE_TYPE in own header EAttributes.h - Put IAttribute in own header IAttribute.h
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@2969 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IAttributes.h
+++ include/IAttributes.h
@@ -23,6 +23,7 @@
 #include "irrString.h"
 #include "irrArray.h"
 #include "IXMLReader.h"
+#include "EAttributes.h"
 
 namespace irr
 {
@@ -33,88 +34,6 @@ namespace video
 namespace io
 {
 	class IXMLWriter;
-
-//! Types of attributes available for IAttributes
-enum E_ATTRIBUTE_TYPE
-{
-	// integer attribute
-	EAT_INT = 0,
-
-	// float attribute
-	EAT_FLOAT,
-
-	// string attribute
-	EAT_STRING,
-
-	// boolean attribute
-	EAT_BOOL,
-
-	// enumeration attribute
-	EAT_ENUM,
-
-	// color attribute
-	EAT_COLOR,
-
-	// floating point color attribute
-	EAT_COLORF,
-
-	// 3d vector attribute
-	EAT_VECTOR3D,
-
-	// 2d position attribute
-	EAT_POSITION2D,
-
-	// vector 2d
-	EAT_VECTOR2D,
-
-	// rectangle attribute
-	EAT_RECT,
-
-	// matrix attribute
-	EAT_MATRIX,
-
-	// quaternion attribute
-	EAT_QUATERNION,
-
-	// 3d bounding box
-	EAT_BBOX,
-
-	// plane
-	EAT_PLANE,
-
-	// 3d triangle
-	EAT_TRIANGLE3D,
-
-	// line 2d
-	EAT_LINE2D,
-
-	// line 3d
-	EAT_LINE3D,
-
-	// array of stringws attribute
-	EAT_STRINGWARRAY,
-
-	// array of float
-	EAT_FLOATARRAY,
-
-	// array of int
-	EAT_INTARRAY,
-
-	// binary data attribute
-	EAT_BINARY,
-
-	// texture reference attribute
-	EAT_TEXTURE,
-
-	// user pointer void*
-	EAT_USER_POINTER,
-
-	// known attribute type count
-	EAT_COUNT,
-
-	// unknown attribute
-	EAT_UNKNOWN
-};
 
 //! Provides a generic interface for attributes and their values and the possiblity to serialize them
 class IAttributes : public virtual IReferenceCounted
