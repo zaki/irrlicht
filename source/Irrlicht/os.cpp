e0bec83ba741746b427cfe4a64c1d4cb8988225c
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-12-04 18:40:28 +0000

    Add openbsd define check.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@2981 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/os.cpp
+++ source/Irrlicht/os.cpp
@@ -19,7 +19,7 @@
 	#include <libkern/OSByteOrder.h>
 	#define bswap_16(X) OSReadSwapInt16(&X,0)
 	#define bswap_32(X) OSReadSwapInt32(&X,0)
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__OpenBSD__)
 	#include <sys/endian.h>
 	#define bswap_16(X) bswap16(X)
 	#define bswap_32(X) bswap32(X)
