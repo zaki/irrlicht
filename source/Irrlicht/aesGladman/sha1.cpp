f9116eab17e991278827f7a357d9b1a29e530c19
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2009-12-04 18:13:19 +0000

    Use irrlicht defines for endianess, and less memory consuming ops.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@2980 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/aesGladman/sha1.cpp
+++ source/Irrlicht/aesGladman/sha1.cpp
@@ -38,6 +38,7 @@
 #include <stdlib.h>     /* for _lrotl with VC++     */
 
 #include "sha1.h"
+#include "../os.h"
 
 /*
     To obtain the highest speed on processors with 32-bit words, this code 
@@ -47,18 +48,6 @@
     It may well fail, in which case the definitions will need to be set by 
     editing at the points marked **** EDIT HERE IF NECESSARY **** below.
 */
-/*  PLATFORM SPECIFIC INCLUDES */
-
-#if defined( __FreeBSD__ ) || defined( __OpenBSD__ )
-#  include <sys/endian.h>
-#elif defined( BSD ) && ( BSD >= 199103 )
-#  include <machine/endian.h>
-#elif defined( __GNUC__ ) || defined( __GNU_LIBRARY__ )
-#  include <endian.h>
-#  include <byteswap.h>
-#elif defined( linux )
-#  include <endian.h>
-#endif
 
 /*  BYTE ORDER IN 32-BIT WORDS
 
@@ -74,85 +63,18 @@
 #define BRG_LITTLE_ENDIAN   1234 /* byte 0 is least significant (i386) */
 #define BRG_BIG_ENDIAN      4321 /* byte 0 is most significant (mc68k) */
 
-#if defined( __alpha__ ) || defined( __alpha ) || defined( i386 )       ||   \
-    defined( __i386__ )  || defined( _M_I86 )  || defined( _M_IX86 )    ||   \
-    defined( __OS2__ )   || defined( sun386 )  || defined( __TURBOC__ ) ||   \
-    defined( vax )       || defined( vms )     || defined( VMS )        ||   \
-    defined( __VMS ) 
-
-#define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-
-#endif
-
-#if defined( AMIGA )    || defined( applec )  || defined( __AS400__ )  ||   \
-    defined( _CRAY )    || defined( __hppa )  || defined( __hp9000 )   ||   \
-    defined( ibm370 )   || defined( mc68000 ) || defined( m68k )       ||   \
-    defined( __MRC__ )  || defined( __MVS__ ) || defined( __MWERKS__ ) ||   \
-    defined( sparc )    || defined( __sparc)  || defined( SYMANTEC_C ) ||   \
-    defined( __TANDEM ) || defined( THINK_C ) || defined( __VMCMS__ )
-    
+#ifdef __BIG_ENDIAN__
 #define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-
-#endif
-
-/*  if the platform is still not known, try to find its byte order  */
-/*  from commonly used definitions in the headers included earlier  */
-
-#if !defined(PLATFORM_BYTE_ORDER)
-
-#if defined(LITTLE_ENDIAN) || defined(BIG_ENDIAN)
-#  if    defined(LITTLE_ENDIAN) && !defined(BIG_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-#  elif !defined(LITTLE_ENDIAN) &&  defined(BIG_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-#  elif defined(BYTE_ORDER) && (BYTE_ORDER == LITTLE_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-#  elif defined(BYTE_ORDER) && (BYTE_ORDER == BIG_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-#  endif
-
-#elif defined(_LITTLE_ENDIAN) || defined(_BIG_ENDIAN)
-#  if    defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-#  elif !defined(_LITTLE_ENDIAN) &&  defined(_BIG_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-#  elif defined(_BYTE_ORDER) && (_BYTE_ORDER == _LITTLE_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-#  elif defined(_BYTE_ORDER) && (_BYTE_ORDER == _BIG_ENDIAN)
-#    define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-#  endif
-
-#elif defined(__LITTLE_ENDIAN__) || defined(__BIG_ENDIAN__)
-#  if    defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)
-#    define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-#  elif !defined(__LITTLE_ENDIAN__) &&  defined(__BIG_ENDIAN__)
-#    define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-#  elif defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __LITTLE_ENDIAN__)
-#    define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-#  elif defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __BIG_ENDIAN__)
-#    define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-#  endif
-
-#elif 0     /* **** EDIT HERE IF NECESSARY **** */
-#define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
-
-#elif 0     /* **** EDIT HERE IF NECESSARY **** */
-#define PLATFORM_BYTE_ORDER BRG_BIG_ENDIAN
-
 #else
-#error Please edit sha1.c (line 141 or 144) to set the platform byte order
-#endif
-
+#define PLATFORM_BYTE_ORDER BRG_LITTLE_ENDIAN
 #endif
 
 #define rotl32(x,n) (((x) << n) | ((x) >> (32 - n)))
 
 #if (PLATFORM_BYTE_ORDER == BRG_BIG_ENDIAN)
 #define swap_b32(x) (x)
-#elif defined(bswap_32)
-#define swap_b32(x) bswap_32(x)
 #else
-#define swap_b32(x) ((rotl32((x), 8) & 0x00ff00ff) | (rotl32((x), 24) & 0xff00ff00))
+#define swap_b32(x) irr::os::Byteswap::byteswap(x)
 #endif
 
 #define SHA1_MASK   (SHA1_BLOCK_SIZE - 1)
