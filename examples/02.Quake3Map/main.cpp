bc2723b2aa527d1f1668a3acb61f0c13b2cfdd6f
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2010-01-28 20:23:27 +0000

    Make driver choice look like the one from the provided header file. Make a variable const.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/branches/releases/1.7@3190 dfc29bdd-3216-0410-991c-e03cc46cb475

--- examples/02.Quake3Map/main.cpp
+++ examples/02.Quake3Map/main.cpp
@@ -57,8 +57,8 @@ int main()
 	video::E_DRIVER_TYPE driverType;
 
 	printf("Please select the driver you want for this example:\n"\
-		" (a) Direct3D 9.0c\n (b) Direct3D 8.1\n (c) OpenGL 1.5\n"\
-		" (d) Software Renderer\n (e) Burning's Software Renderer\n"\
+		" (a) OpenGL 1.5\n (b) Direct3D 9.0c\n (c) Direct3D 8.1\n"\
+		" (d) Burning's Software Renderer\n (e) Software Renderer\n"\
 		" (f) NullDevice\n (otherKey) exit\n\n");
 
 	char i;
@@ -66,11 +66,11 @@ int main()
 
 	switch(i)
 	{
-		case 'a': driverType = video::EDT_DIRECT3D9;break;
-		case 'b': driverType = video::EDT_DIRECT3D8;break;
-		case 'c': driverType = video::EDT_OPENGL;   break;
-		case 'd': driverType = video::EDT_SOFTWARE; break;
-		case 'e': driverType = video::EDT_BURNINGSVIDEO;break;
+		case 'a': driverType = video::EDT_OPENGL;   break;
+		case 'b': driverType = video::EDT_DIRECT3D9;break;
+		case 'c': driverType = video::EDT_DIRECT3D8;break;
+		case 'd': driverType = video::EDT_BURNINGSVIDEO;break;
+		case 'e': driverType = video::EDT_SOFTWARE; break;
 		case 'f': driverType = video::EDT_NULL;     break;
 		default: return 1;
 	}
