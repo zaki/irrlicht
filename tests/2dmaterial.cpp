046ad73003cc03067a579289951273aeab1f88e7
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2010-09-12 11:05:32 +0000

    Add gui image for 2dmaterial test. Enable anti-aliasing in aa test.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@3406 dfc29bdd-3216-0410-991c-e03cc46cb475

--- tests/2dmaterial.cpp
+++ tests/2dmaterial.cpp
@@ -50,48 +50,20 @@ static bool addBlend2d(video::E_DRIVER_TYPE type)
 		gui::IGUITab* t1 = tab->addTab(L"Config");
 
 		// add some edit boxes and a button to tab one
-		env->addStaticText(L"Scale:",
-				core::rect<s32>(10,20,60,45), false, false, t1);
+		env->addImage(driver->getTexture("../media/tools.png"), core::vector2d<s32>(10,20), true, t1);
 		env->addStaticText(L"X:", core::rect<s32>(22,48,40,66), false, false, t1);
 		env->addEditBox(L"1.0", core::rect<s32>(40,46,130,66), true, t1, 201);
-		env->addStaticText(L"Y:", core::rect<s32>(22,82,40,96), false, false, t1);
-		env->addEditBox(L"1.0", core::rect<s32>(40,76,130,96), true, t1, 202);
-		env->addStaticText(L"Z:", core::rect<s32>(22,108,40,126), false, false, t1);
-		env->addEditBox(L"1.0", core::rect<s32>(40,106,130,126), true, t1, 203);
-
-		env->addButton(core::rect<s32>(10,134,85,165), t1, 101, L"Set");
 
 		// quick scale buttons
 		env->addButton(core::rect<s32>(65,20,95,40), t1, 102, L"* 10");
 		env->addButton(core::rect<s32>(100,20,130,40), t1, 103, L"* 0.1");
-
-		// add transparency control
-		env->addStaticText(L"GUI Transparency Control:",
-				core::rect<s32>(10,200,150,225), true, false, t1);
-		gui::IGUIScrollBar* scrollbar = env->addScrollBar(true,
-				core::rect<s32>(10,225,150,240), t1, 104);
-		scrollbar->setMax(255);
-		scrollbar->setPos(255);
-
-		// add framerate control
-		env->addStaticText(L":", core::rect<s32>(10,240,150,265), true, false, t1);
-		env->addStaticText(L"Framerate:",
-				core::rect<s32>(12,240,75,265), false, false, t1);
-		env->addStaticText(L"", core::rect<s32>(75,240,200,265), false, false, t1,
-				105);
-		scrollbar = env->addScrollBar(true,
-				core::rect<s32>(10,265,150,280), t1, 106);
-		scrollbar->setMax(100);
-		scrollbar->setMin(-100);
-		scrollbar->setPos(20);
-		scrollbar->setSmallStep(1);
 	}
+
 	video::SMaterial& material2D = driver->getMaterial2D();
 	material2D.setFlag(video::EMF_ANTI_ALIASING, true);
 	for (unsigned int n=0; n<video::MATERIAL_MAX_TEXTURES; n++)
 	{
 		material2D.TextureLayer[n].BilinearFilter = true;
-		//material2D.TextureLayer[n].TextureWrap = ETC_CLAMP_TO_EDGE;
 		material2D.TextureLayer[n].TextureWrapU = video::ETC_CLAMP_TO_EDGE;
 		material2D.TextureLayer[n].TextureWrapV = video::ETC_CLAMP_TO_EDGE;
 		material2D.TextureLayer[n].LODBias = 8;
@@ -105,7 +77,7 @@ static bool addBlend2d(video::E_DRIVER_TYPE type)
 	driver->enableMaterial2D(false);
 	driver->endScene();
 
-	bool result = takeScreenshotAndCompareAgainstReference(driver, "-addBlend2D.png", 98.66f);
+	bool result = takeScreenshotAndCompareAgainstReference(driver, "-addBlend2D.png", 99.8f);
 
 	device->drop();
     return result;
@@ -115,5 +87,7 @@ bool twodmaterial()
 {
 	bool result = addBlend2d(video::EDT_OPENGL);
 	result &= addBlend2d(video::EDT_DIRECT3D9);
+	result &= addBlend2d(video::EDT_DIRECT3D8);
+	result &= addBlend2d(video::EDT_BURNINGSVIDEO);
 	return result;
 }
