69bc11c8f565f9c4becce866e0427e408bb449e3
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2010-09-13 11:24:14 +0000

    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@3409 dfc29bdd-3216-0410-991c-e03cc46cb475

--- tests/drawRectOutline.cpp
+++ tests/drawRectOutline.cpp
@@ -2,10 +2,13 @@
 
 using namespace irr;
 
-bool drawRectOutline(void)
+bool testWithDriver(video::E_DRIVER_TYPE driverType)
 {
 	IrrlichtDevice *device =
-		createDevice(video::EDT_BURNINGSVIDEO, core::dimension2du(160, 120));
+		createDevice(driverType, core::dimension2du(160, 120));
+	if (!device)
+		return true;
+
 	video::IVideoDriver* driver = device->getVideoDriver();
 
 	driver->beginScene(true, true, video::SColor(255,100,101,140));
@@ -18,11 +21,27 @@ bool drawRectOutline(void)
 	r += core::position2di( 10 , 10 );
 	driver->draw2DRectangleOutline( r , video::SColor(128, 255, 128, 128) );
 
+	driver->getMaterial2D().Thickness=12.f;
+	driver->enableMaterial2D();
+	r += core::position2di( 10 , 10 );
+	driver->draw2DRectangleOutline( r , video::SColor(128, 255, 128, 128) );
+
 	driver->endScene();
 
-	bool result = takeScreenshotAndCompareAgainstReference(driver, "-drawRectOutline.png" );
+	bool result = takeScreenshotAndCompareAgainstReference(driver, "-drawRectOutline.png", 99.9f );
 
 	device->drop();
 
 	return result ;
+}
+
+bool drawRectOutline(void)
+{
+	// TODO: Only OpenGL supports thick lines
+	bool result = testWithDriver(video::EDT_BURNINGSVIDEO);
+	result &= testWithDriver(video::EDT_DIRECT3D8);
+	result &= testWithDriver(video::EDT_DIRECT3D9);
+	result &= testWithDriver(video::EDT_OPENGL);
+	result &= testWithDriver(video::EDT_SOFTWARE);
+	return result;
 }
