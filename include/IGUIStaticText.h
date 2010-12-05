e877b786c7c21fd7f9df67be16e6847c41a7320a
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2010-12-05 15:19:51 +0000

    Patch by Auria, which allows to render text in gui static text elements over the element clip area. This makes handling of changing text simpler. As default, this feature is turned off.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@3483 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IGUIStaticText.h
+++ include/IGUIStaticText.h
@@ -91,6 +91,12 @@ namespace gui
 		/** If the text is broken, this returns the width of the widest line
 		\return The width of the text, or the widest broken line. */
 		virtual s32 getTextWidth(void) const = 0;
+		
+		//! Set whether the text in this label should be clipped if it goes outside bounds
+		virtual void setTextRestrainedInside(bool restrainedInside) = 0;
+		
+		//! Checks if the text in this label should be clipped if it goes outside bounds
+		virtual bool isTextRestrainedInside() const = 0;
 	};
 
 
