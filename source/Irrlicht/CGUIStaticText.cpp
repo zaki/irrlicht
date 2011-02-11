c9a101c0441f20f9bddacf91f0db7486045ed1b3
bitplane <bitplane@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-02-11 08:13:52 +0000

    Fix valgrind complaints about uninitialized variables in RTL patch
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@3592 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CGUIStaticText.cpp
+++ source/Irrlicht/CGUIStaticText.cpp
@@ -24,6 +24,7 @@ CGUIStaticText::CGUIStaticText(const wchar_t* text, bool border,
 : IGUIStaticText(environment, parent, id, rectangle),
 	HAlign(EGUIA_UPPERLEFT), VAlign(EGUIA_UPPERLEFT),
 	Border(border), OverrideColorEnabled(false), OverrideBGColorEnabled(false), WordWrap(false), Background(background),
+	RestrainTextInside(true), RightToLeft(false),
 	OverrideColor(video::SColor(101,255,255,255)), BGColor(video::SColor(101,210,210,210)),
 	OverrideFont(0), LastBreakFont(0)
 {
@@ -32,7 +33,6 @@ CGUIStaticText::CGUIStaticText(const wchar_t* text, bool border,
 	#endif
 
 	Text = text;
-	RestrainTextInside = true;
 	if (environment && environment->getSkin())
 	{
 		BGColor = environment->getSkin()->getColor(gui::EGDC_3D_FACE);
