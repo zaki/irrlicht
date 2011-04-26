617d5a4e3a57afa50629799dc6ae2bca97154fef
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-04-26 14:23:32 +0000

    Fix default blend mode to EBO_NONE, in order to avoid unintended blending
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@3660 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/SMaterial.h
+++ include/SMaterial.h
@@ -219,7 +219,7 @@ namespace video
 			EmissiveColor(0,0,0,0), SpecularColor(255,255,255,255),
 			Shininess(0.0f), MaterialTypeParam(0.0f), MaterialTypeParam2(0.0f), Thickness(1.0f),
 			ZBuffer(ECFN_LESSEQUAL), AntiAliasing(EAAM_SIMPLE), ColorMask(ECP_ALL),
-			ColorMaterial(ECM_DIFFUSE), BlendOperation(EBO_ADD),
+			ColorMaterial(ECM_DIFFUSE), BlendOperation(EBO_NONE),
 			PolygonOffsetFactor(0), PolygonOffsetDirection(EPO_FRONT),
 			Wireframe(false), PointCloud(false), GouraudShading(true),
 			Lighting(true), ZWriteEnable(true), BackfaceCulling(true), FrontfaceCulling(false),
