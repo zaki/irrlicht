4424ffec5f3b2dac8e8b578a6b2c7f2951c06635
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-04-28 11:32:37 +0000

    Support new blend op from recent AMD extension. OpenGL/AMD only for now.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@3679 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/SMaterial.h
+++ include/SMaterial.h
@@ -22,15 +22,15 @@ namespace video
 	//! Flag for EMT_ONETEXTURE_BLEND, ( BlendFactor ) BlendFunc = source * sourceFactor + dest * destFactor
 	enum E_BLEND_FACTOR
 	{
-		EBF_ZERO	= 0,		//!< src & dest	(0, 0, 0, 0)
-		EBF_ONE,			//!< src & dest	(1, 1, 1, 1)
-		EBF_DST_COLOR, 			//!< src	(destR, destG, destB, destA)
-		EBF_ONE_MINUS_DST_COLOR, 	//!< src	(1-destR, 1-destG, 1-destB, 1-destA)
-		EBF_SRC_COLOR,			//!< dest	(srcR, srcG, srcB, srcA)
+		EBF_ZERO	= 0,			//!< src & dest	(0, 0, 0, 0)
+		EBF_ONE,					//!< src & dest	(1, 1, 1, 1)
+		EBF_DST_COLOR, 				//!< src	(destR, destG, destB, destA)
+		EBF_ONE_MINUS_DST_COLOR,	//!< src	(1-destR, 1-destG, 1-destB, 1-destA)
+		EBF_SRC_COLOR,				//!< dest	(srcR, srcG, srcB, srcA)
 		EBF_ONE_MINUS_SRC_COLOR, 	//!< dest	(1-srcR, 1-srcG, 1-srcB, 1-srcA)
-		EBF_SRC_ALPHA,			//!< src & dest	(srcA, srcA, srcA, srcA)
+		EBF_SRC_ALPHA,				//!< src & dest	(srcA, srcA, srcA, srcA)
 		EBF_ONE_MINUS_SRC_ALPHA,	//!< src & dest	(1-srcA, 1-srcA, 1-srcA, 1-srcA)
-		EBF_DST_ALPHA,			//!< src & dest	(destA, destA, destA, destA)
+		EBF_DST_ALPHA,				//!< src & dest	(destA, destA, destA, destA)
 		EBF_ONE_MINUS_DST_ALPHA,	//!< src & dest	(1-destA, 1-destA, 1-destA, 1-destA)
 		EBF_SRC_ALPHA_SATURATE		//!< src	(min(srcA, 1-destA), idem, ...)
 	};
@@ -43,7 +43,9 @@ namespace video
 		EBO_SUBTRACT,	//!< This mode subtracts the color values
 		EBO_REVSUBTRACT,//!< This modes subtracts destination from source
 		EBO_MIN,		//!< Choose minimum value of each color channel
-		EBO_MAX			//!< Choose maximum value of each color channel
+		EBO_MAX,		//!< Choose maximum value of each color channel
+		EBO_MIN_FACTOR,	//!< Choose minimum value of each color channel after applying blend factors, not widely supported
+		EBO_MAX_FACTOR	//!< Choose maximum value of each color channel after applying blend factors, not widely supported
 	};
 
 	//! MaterialTypeParam: e.g. DirectX: D3DTOP_MODULATE, D3DTOP_MODULATE2X, D3DTOP_MODULATE4X
