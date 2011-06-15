6296a1c1ffb1b716a4600b870a114900bd825eb1
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-06-15 15:28:02 +0000

    ColladaMeshWriter - add infrastructure to export transparent textures.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@3851 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IColladaMeshWriter.h
+++ include/IColladaMeshWriter.h
@@ -17,7 +17,7 @@ namespace io
 
 namespace scene
 {
-	 // Lighting models - more or less the way Collada categorizes materials
+	//! Lighting models - more or less the way Collada categorizes materials
 	enum E_COLLADA_TECHNIQUE_FX
 	{
 		//! Blinn-phong which is default for opengl and dx fixed function pipelines.
@@ -31,6 +31,38 @@ namespace scene
 		ECTF_CONSTANT
 	};
 
+	//! How to export opacity to collada
+	/** Note that there is also a check for transparent textures in IColladaMeshWriterProperties
+	which will be evaluated additionally to the flags:
+	No ECOF_TRANSPARENT_* flag and no texture => <transparent> will not be written at all.
+	No ECOF_TRANSPARENT_* flag but a valid texture => texture will be written.
+	ECOF_TRANSPARENT_* flag is set and no texture => A color will be written.
+	Both, a ECOF_TRANSPARENT_* flag and a valid texture => only the texture will be written.
+
+	You can only have either ECOF_A_ONE or ECOF_RGB_ZERO otherwise ECOF_RGB_ZERO is used.
+	You can only have one of the ECOF_TRANSPARENT_* flags otherwise the first is used.
+	*/
+	enum E_COLLADA_TRANSPARENT_FX
+	{
+		//! default - only alpha channel of color or texture is used. Can't be used together with ECOF_RGB_ZERO.
+		ECOF_A_ONE = 0,
+
+		//! Alpha values for each RGB channel of color or texture are used. Can't be used together with ECOF_A_ONE.
+		ECOF_RGB_ZERO = 1,
+
+		//! Write diffuse material color as transparent
+		ECOF_TRANSPARENT_DIFFUSE = 2,
+
+		//! Write ambient material color as transparent
+		ECOF_TRANSPARENT_AMBIENT = 4,
+
+		//! Write emissive material color as transparent
+		ECOF_TRANSPARENT_EMISSIVE = 8, 
+
+		//! Write specular material color as transparent
+		ECOF_TRANSPARENT_SPECULAR = 16,
+	};
+
 	//! Callback interface for properties which can be used to influence collada writing
 	class IColladaMeshWriterProperties  : public virtual IReferenceCounted
 	{
@@ -40,22 +72,35 @@ namespace scene
 		//! Which lighting model should be used in the technique (FX) section when exporting effects (materials)
 		virtual E_COLLADA_TECHNIQUE_FX getTechniqueFx(const video::SMaterial& material) const = 0;
 
-		//! Which texture index should be used when setting the emissive texture
+		//! Which texture index should be used when writing the emissive texture
 		/** \return the index to the texture-layer or -1 if that texture should never be exported */
 		virtual s32 getEmissiveTextureIdx(const video::SMaterial& material) const	= 0;
 
-		//! Which texture index should be used when setting the ambient texture
+		//! Which texture index should be used when writing the ambient texture
 		/** \return the index to the texture-layer or -1 if that texture should never be exported */
 		virtual s32 getAmbientTextureIdx(const video::SMaterial& material) const 	= 0;
 
-		//! Which texture index should be used when setting the diffuse texture
+		//! Which texture index should be used when writing the diffuse texture
 		/** \return the index to the texture-layer or -1 if that texture should never be exported */
 		virtual s32 getDiffuseTextureIdx(const video::SMaterial& material) const 	= 0;
 
-		//! Which texture index should be used when setting the specular texture
+		//! Which texture index should be used when writing the specular texture
 		/** \return the index to the texture-layer or -1 if that texture should never be exported */
 		virtual s32 getSpecularTextureIdx(const video::SMaterial& material) const 	= 0;
 
+		//! Which texture index should be used when writing the transparent texture
+		/** Note: By default the alpha channel is used, if you want to use RGB you have to set
+		the ECOF_RGB_ZERO flag in getTransparentFx.
+		\return the index to the texture-layer or -1 if that texture should never be exported */
+		virtual s32 getTransparentTextureIdx( const video::SMaterial& material) const 	= 0;
+
+		//! Return the settings for transparence
+		virtual E_COLLADA_TRANSPARENT_FX getTransparentFx(const video::SMaterial& material) const = 0;
+
+		//! Transparency value for the material. 
+		/** This value is additional to transparent settings, if both are set they will be multiplicated.
+		\return 1.0 for fully transparent, 0.0 for not transparent and not written at all when < 0.f */
+		virtual f32 getTransparency(const video::SMaterial& material) const = 0;
 	};
 
 
