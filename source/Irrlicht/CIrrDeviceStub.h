df8f20fb875f16aef91a2b1ba4297a2d8fe52e5c
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-06-21 22:02:18 +0000

    Add access and changeability to internal Randomizer.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@3864 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CIrrDeviceStub.h
+++ source/Irrlicht/CIrrDeviceStub.h
@@ -15,6 +15,7 @@ namespace irr
 	// lots of prototypes:
 	class ILogger;
 	class CLogger;
+	class IRandomizer;
 
 	namespace gui
 	{
@@ -97,6 +98,15 @@ namespace irr
 		//! Returns a pointer to the logger.
 		virtual ILogger* getLogger();
 
+		//! Provides access to the engine's currently set randomizer.
+		virtual IRandomizer* getRandomizer() const;
+
+		//! Sets a new randomizer.
+		virtual void setRandomizer(IRandomizer* r);
+
+		//! Creates a new default randomizer.
+		virtual IRandomizer* createDefaultRandomizer() const;
+
 		//! Returns the operation system opertator object.
 		virtual IOSOperator* getOSOperator();
 
@@ -148,6 +158,7 @@ namespace irr
 		IEventReceiver* UserReceiver;
 		CLogger* Logger;
 		IOSOperator* Operator;
+		IRandomizer* Randomizer;
 		io::IFileSystem* FileSystem;
 		scene::ISceneManager* InputReceivingSceneManager;
 
