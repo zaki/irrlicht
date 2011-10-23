ba4172de61583f1b832e25407bdef7e4597ab0a1
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-10-23 00:07:39 +0000

    OSX input patch by Auria
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@3959 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/MacOSX/CIrrDeviceMacOSX.mm
+++ source/Irrlicht/MacOSX/CIrrDeviceMacOSX.mm
@@ -42,14 +42,153 @@
 #include <IOKit/hid/IOHIDLib.h>
 #include <IOKit/hid/IOHIDKeys.h>
 
+// Contents from Events.h from Carbon/HIToolbox but we need it with Cocoa too
+// and for some reason no Cocoa equivalent of these constants seems provided.
+// So I'm doing like everyone else and using copy-and-paste.
+
+/*
+ *  Summary:
+ *	Virtual keycodes
+ *  
+ *  Discussion:
+ *	These constants are the virtual keycodes defined originally in
+ *	Inside Mac Volume V, pg. V-191. They identify physical keys on a
+ *	keyboard. Those constants with "ANSI" in the name are labeled
+ *	according to the key position on an ANSI-standard US keyboard.
+ *	For example, kVK_ANSI_A indicates the virtual keycode for the key
+ *	with the letter 'A' in the US keyboard layout. Other keyboard
+ *	layouts may have the 'A' key label on a different physical key;
+ *	in this case, pressing 'A' will generate a different virtual
+ *	keycode.
+ */
+enum {
+	kVK_ANSI_A              = 0x00,
+	kVK_ANSI_S              = 0x01,
+	kVK_ANSI_D              = 0x02,
+	kVK_ANSI_F              = 0x03,
+	kVK_ANSI_H              = 0x04,
+	kVK_ANSI_G              = 0x05,
+	kVK_ANSI_Z              = 0x06,
+	kVK_ANSI_X              = 0x07,
+	kVK_ANSI_C              = 0x08,
+	kVK_ANSI_V              = 0x09,
+	kVK_ANSI_B              = 0x0B,
+	kVK_ANSI_Q              = 0x0C,
+	kVK_ANSI_W              = 0x0D,
+	kVK_ANSI_E              = 0x0E,
+	kVK_ANSI_R              = 0x0F,
+	kVK_ANSI_Y              = 0x10,
+	kVK_ANSI_T              = 0x11,
+	kVK_ANSI_1              = 0x12,
+	kVK_ANSI_2              = 0x13,
+	kVK_ANSI_3              = 0x14,
+	kVK_ANSI_4              = 0x15,
+	kVK_ANSI_6              = 0x16,
+	kVK_ANSI_5              = 0x17,
+	kVK_ANSI_Equal          = 0x18,
+	kVK_ANSI_9              = 0x19,
+	kVK_ANSI_7              = 0x1A,
+	kVK_ANSI_Minus          = 0x1B,
+	kVK_ANSI_8              = 0x1C,
+	kVK_ANSI_0              = 0x1D,
+	kVK_ANSI_RightBracket   = 0x1E,
+	kVK_ANSI_O              = 0x1F,
+	kVK_ANSI_U              = 0x20,
+	kVK_ANSI_LeftBracket    = 0x21,
+	kVK_ANSI_I              = 0x22,
+	kVK_ANSI_P              = 0x23,
+	kVK_ANSI_L              = 0x25,
+	kVK_ANSI_J              = 0x26,
+	kVK_ANSI_Quote          = 0x27,
+	kVK_ANSI_K              = 0x28,
+	kVK_ANSI_Semicolon      = 0x29,
+	kVK_ANSI_Backslash      = 0x2A,
+	kVK_ANSI_Comma          = 0x2B,
+	kVK_ANSI_Slash          = 0x2C,
+	kVK_ANSI_N              = 0x2D,
+	kVK_ANSI_M              = 0x2E,
+	kVK_ANSI_Period         = 0x2F,
+	kVK_ANSI_Grave          = 0x32,
+	kVK_ANSI_KeypadDecimal  = 0x41,
+	kVK_ANSI_KeypadMultiply = 0x43,
+	kVK_ANSI_KeypadPlus     = 0x45,
+	kVK_ANSI_KeypadClear    = 0x47,
+	kVK_ANSI_KeypadDivide   = 0x4B,
+	kVK_ANSI_KeypadEnter    = 0x4C,
+	kVK_ANSI_KeypadMinus    = 0x4E,
+	kVK_ANSI_KeypadEquals   = 0x51,
+	kVK_ANSI_Keypad0        = 0x52,
+	kVK_ANSI_Keypad1        = 0x53,
+	kVK_ANSI_Keypad2        = 0x54,
+	kVK_ANSI_Keypad3        = 0x55,
+	kVK_ANSI_Keypad4        = 0x56,
+	kVK_ANSI_Keypad5        = 0x57,
+	kVK_ANSI_Keypad6        = 0x58,
+	kVK_ANSI_Keypad7        = 0x59,
+	kVK_ANSI_Keypad8        = 0x5B,
+	kVK_ANSI_Keypad9        = 0x5C
+};
+
+/* keycodes for keys that are independent of keyboard layout*/
+enum {
+	kVK_Return        = 0x24,
+	kVK_Tab           = 0x30,
+	kVK_Space         = 0x31,
+	kVK_Delete        = 0x33,
+	kVK_Escape        = 0x35,
+	kVK_Command       = 0x37,
+	kVK_Shift         = 0x38,
+	kVK_CapsLock      = 0x39,
+	kVK_Option        = 0x3A,
+	kVK_Control       = 0x3B,
+	kVK_RightShift    = 0x3C,
+	kVK_RightOption   = 0x3D,
+	kVK_RightControl  = 0x3E,
+	kVK_Function      = 0x3F,
+	kVK_F17           = 0x40,
+	kVK_VolumeUp      = 0x48,
+	kVK_VolumeDown    = 0x49,
+	kVK_Mute          = 0x4A,
+	kVK_F18           = 0x4F,
+	kVK_F19           = 0x50,
+	kVK_F20           = 0x5A,
+	kVK_F5            = 0x60,
+	kVK_F6            = 0x61,
+	kVK_F7            = 0x62,
+	kVK_F3            = 0x63,
+	kVK_F8            = 0x64,
+	kVK_F9            = 0x65,
+	kVK_F11           = 0x67,
+	kVK_F13           = 0x69,
+	kVK_F16           = 0x6A,
+	kVK_F14           = 0x6B,
+	kVK_F10           = 0x6D,
+	kVK_F12           = 0x6F,
+	kVK_F15           = 0x71,
+	kVK_Help          = 0x72,
+	kVK_Home          = 0x73,
+	kVK_PageUp        = 0x74,
+	kVK_ForwardDelete = 0x75,
+	kVK_F4            = 0x76,
+	kVK_End           = 0x77,
+	kVK_F2            = 0x78,
+	kVK_PageDown      = 0x79,
+	kVK_F1            = 0x7A,
+	kVK_LeftArrow     = 0x7B,
+	kVK_RightArrow    = 0x7C,
+	kVK_DownArrow     = 0x7D,
+	kVK_UpArrow       = 0x7E
+};
+
+
 struct JoystickComponent
 {
-	IOHIDElementCookie cookie;				// unique value which identifies element, will NOT change
-	long min;								// reported min value possible
-	long max;								// reported max value possible
+	IOHIDElementCookie cookie; // unique value which identifies element, will NOT change
+	long min; // reported min value possible
+	long max; // reported max value possible
 
-	long minRead;							//min read value
-	long maxRead;							//max read value
+	long minRead; //min read value
+	long maxRead; //max read value
 
 	JoystickComponent() : min(0), minRead(0), max(0), maxRead(0)
 	{
@@ -929,9 +1068,12 @@ void CIrrDeviceMacOSX::postKeyEvent(void *event,irr::SEvent &ievent,bool pressed
 		mkey = mchar = 0;
 		skipCommand = false;
 		c = [str characterAtIndex:0];
+		mchar = c;
 
-		iter = KeyCodes.find(c);
+		iter = KeyCodes.find([(NSEvent *)event keyCode]);
 		if (iter != KeyCodes.end())
+			mkey = (*iter).second;
+		else if ((iter = KeyCodes.find(c))  != KeyCodes.end())
 			mkey = (*iter).second;
 		else
 		{
@@ -1064,42 +1206,90 @@ void CIrrDeviceMacOSX::setCursorVisible(bool visible)
 
 void CIrrDeviceMacOSX::initKeycodes()
 {
-	KeyCodes[NSUpArrowFunctionKey]     = irr::KEY_UP;
-	KeyCodes[NSDownArrowFunctionKey]   = irr::KEY_DOWN;
-	KeyCodes[NSLeftArrowFunctionKey]   = irr::KEY_LEFT;
-	KeyCodes[NSRightArrowFunctionKey]  = irr::KEY_RIGHT;
-	KeyCodes[NSF1FunctionKey]          = irr::KEY_F1;
-	KeyCodes[NSF2FunctionKey]          = irr::KEY_F2;
-	KeyCodes[NSF3FunctionKey]          = irr::KEY_F3;
-	KeyCodes[NSF4FunctionKey]          = irr::KEY_F4;
-	KeyCodes[NSF5FunctionKey]          = irr::KEY_F5;
-	KeyCodes[NSF6FunctionKey]          = irr::KEY_F6;
-	KeyCodes[NSF7FunctionKey]          = irr::KEY_F7;
-	KeyCodes[NSF8FunctionKey]          = irr::KEY_F8;
-	KeyCodes[NSF9FunctionKey]          = irr::KEY_F9;
-	KeyCodes[NSF10FunctionKey]         = irr::KEY_F10;
-	KeyCodes[NSF11FunctionKey]         = irr::KEY_F11;
-	KeyCodes[NSF12FunctionKey]         = irr::KEY_F12;
-	KeyCodes[NSF13FunctionKey]         = irr::KEY_F13;
-	KeyCodes[NSF14FunctionKey]         = irr::KEY_F14;
-	KeyCodes[NSF15FunctionKey]         = irr::KEY_F15;
-	KeyCodes[NSF16FunctionKey]         = irr::KEY_F16;
-	KeyCodes[NSHomeFunctionKey]        = irr::KEY_HOME;
-	KeyCodes[NSEndFunctionKey]         = irr::KEY_END;
+	KeyCodes[kVK_UpArrow]     = irr::KEY_UP;
+	KeyCodes[kVK_DownArrow]   = irr::KEY_DOWN;
+	KeyCodes[kVK_LeftArrow]   = irr::KEY_LEFT;
+	KeyCodes[kVK_RightArrow]  = irr::KEY_RIGHT;
+	KeyCodes[kVK_F1]          = irr::KEY_F1;
+	KeyCodes[kVK_F2]          = irr::KEY_F2;
+	KeyCodes[kVK_F3]          = irr::KEY_F3;
+	KeyCodes[kVK_F4]          = irr::KEY_F4;
+	KeyCodes[kVK_F5]          = irr::KEY_F5;
+	KeyCodes[kVK_F6]          = irr::KEY_F6;
+	KeyCodes[kVK_F7]          = irr::KEY_F7;
+	KeyCodes[kVK_F8]          = irr::KEY_F8;
+	KeyCodes[kVK_F9]          = irr::KEY_F9;
+	KeyCodes[kVK_F10]         = irr::KEY_F10;
+	KeyCodes[kVK_F11]         = irr::KEY_F11;
+	KeyCodes[kVK_F12]         = irr::KEY_F12;
+	KeyCodes[kVK_F13]         = irr::KEY_F13;
+	KeyCodes[kVK_F14]         = irr::KEY_F14;
+	KeyCodes[kVK_F15]         = irr::KEY_F15;
+	KeyCodes[kVK_F16]         = irr::KEY_F16;
+	KeyCodes[kVK_F17]         = irr::KEY_F17;
+	KeyCodes[kVK_F18]         = irr::KEY_F18;
+	KeyCodes[kVK_F19]         = irr::KEY_F19;
+	KeyCodes[kVK_F20]         = irr::KEY_F20;
+	KeyCodes[kVK_Home]        = irr::KEY_HOME;
+	KeyCodes[kVK_End]         = irr::KEY_END;
 	KeyCodes[NSInsertFunctionKey]      = irr::KEY_INSERT;
-	KeyCodes[NSDeleteFunctionKey]      = irr::KEY_DELETE;
-	KeyCodes[NSHelpFunctionKey]        = irr::KEY_HELP;
+	KeyCodes[kVK_ForwardDelete]        = irr::KEY_DELETE;
+	KeyCodes[kVK_Help]                 = irr::KEY_HELP;
 	KeyCodes[NSSelectFunctionKey]      = irr::KEY_SELECT;
 	KeyCodes[NSPrintFunctionKey]       = irr::KEY_PRINT;
 	KeyCodes[NSExecuteFunctionKey]     = irr::KEY_EXECUT;
 	KeyCodes[NSPrintScreenFunctionKey] = irr::KEY_SNAPSHOT;
 	KeyCodes[NSPauseFunctionKey]       = irr::KEY_PAUSE;
 	KeyCodes[NSScrollLockFunctionKey]  = irr::KEY_SCROLL;
-	KeyCodes[0x7F]                     = irr::KEY_BACK;
-	KeyCodes[0x09]                     = irr::KEY_TAB;
-	KeyCodes[0x0D]                     = irr::KEY_RETURN;
-	KeyCodes[0x03]                     = irr::KEY_RETURN;
-	KeyCodes[0x1B]                     = irr::KEY_ESCAPE;
+	KeyCodes[kVK_Delete]               = irr::KEY_BACK;
+	KeyCodes[kVK_Tab]                  = irr::KEY_TAB;
+	KeyCodes[kVK_Return]               = irr::KEY_RETURN;
+	KeyCodes[kVK_Escape]               = irr::KEY_ESCAPE;
+	KeyCodes[kVK_Control]              = irr::KEY_CONTROL;
+	KeyCodes[kVK_RightControl]         = irr::KEY_RCONTROL;
+	KeyCodes[kVK_Command]              = irr::KEY_MENU;
+	KeyCodes[kVK_Shift]                = irr::KEY_SHIFT;
+	KeyCodes[kVK_RightShift]           = irr::KEY_RSHIFT;
+	KeyCodes[kVK_Space]                = irr::KEY_SPACE;
+   
+	KeyCodes[kVK_ANSI_A] = irr::KEY_KEY_A;
+	KeyCodes[kVK_ANSI_B] = irr::KEY_KEY_B;
+	KeyCodes[kVK_ANSI_C] = irr::KEY_KEY_C;
+	KeyCodes[kVK_ANSI_D] = irr::KEY_KEY_D;
+	KeyCodes[kVK_ANSI_E] = irr::KEY_KEY_E;
+	KeyCodes[kVK_ANSI_F] = irr::KEY_KEY_F;
+	KeyCodes[kVK_ANSI_G] = irr::KEY_KEY_G;
+	KeyCodes[kVK_ANSI_H] = irr::KEY_KEY_H;
+	KeyCodes[kVK_ANSI_I] = irr::KEY_KEY_I;
+	KeyCodes[kVK_ANSI_J] = irr::KEY_KEY_J;
+	KeyCodes[kVK_ANSI_K] = irr::KEY_KEY_K;
+	KeyCodes[kVK_ANSI_L] = irr::KEY_KEY_L;
+	KeyCodes[kVK_ANSI_M] = irr::KEY_KEY_M;
+	KeyCodes[kVK_ANSI_N] = irr::KEY_KEY_N;
+	KeyCodes[kVK_ANSI_O] = irr::KEY_KEY_O;
+	KeyCodes[kVK_ANSI_P] = irr::KEY_KEY_P;
+	KeyCodes[kVK_ANSI_Q] = irr::KEY_KEY_Q;
+	KeyCodes[kVK_ANSI_R] = irr::KEY_KEY_R;
+	KeyCodes[kVK_ANSI_S] = irr::KEY_KEY_S;
+	KeyCodes[kVK_ANSI_T] = irr::KEY_KEY_T;
+	KeyCodes[kVK_ANSI_U] = irr::KEY_KEY_U;
+	KeyCodes[kVK_ANSI_V] = irr::KEY_KEY_V;
+	KeyCodes[kVK_ANSI_W] = irr::KEY_KEY_W;
+	KeyCodes[kVK_ANSI_X] = irr::KEY_KEY_X;
+	KeyCodes[kVK_ANSI_X] = irr::KEY_KEY_X;
+	KeyCodes[kVK_ANSI_Y] = irr::KEY_KEY_Y;
+	KeyCodes[kVK_ANSI_Z] = irr::KEY_KEY_Z;
+	
+	KeyCodes[kVK_ANSI_0] = irr::KEY_KEY_0;
+	KeyCodes[kVK_ANSI_1] = irr::KEY_KEY_1;
+	KeyCodes[kVK_ANSI_2] = irr::KEY_KEY_2;
+	KeyCodes[kVK_ANSI_3] = irr::KEY_KEY_3;
+	KeyCodes[kVK_ANSI_4] = irr::KEY_KEY_4;
+	KeyCodes[kVK_ANSI_5] = irr::KEY_KEY_5;
+	KeyCodes[kVK_ANSI_6] = irr::KEY_KEY_6;
+	KeyCodes[kVK_ANSI_7] = irr::KEY_KEY_7;
+	KeyCodes[kVK_ANSI_8] = irr::KEY_KEY_8;
+	KeyCodes[kVK_ANSI_9] = irr::KEY_KEY_9;
 }
 
 
