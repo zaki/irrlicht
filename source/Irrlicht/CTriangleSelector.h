dda427dd99ff822dcdc1a0aac98c3b4efba4d5fc
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-11-01 21:10:01 +0000

    Triangle selector improvements suggested by rogerborg a long time ago. Now fixed and properly working.
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@3984 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CTriangleSelector.h
+++ source/Irrlicht/CTriangleSelector.h
@@ -8,6 +8,7 @@
 #include "ITriangleSelector.h"
 #include "IMesh.h"
 #include "irrArray.h"
+#include "aabbox3d.h"
 
 namespace irr
 {
@@ -77,6 +78,7 @@ protected:
 
 	ISceneNode* SceneNode;
 	mutable core::array<core::triangle3df> Triangles; // (mutable for CTriangleBBSelector)
+	mutable core::aabbox3df BoundingBox; // Allows for trivial rejection
 
 	IAnimatedMeshSceneNode* AnimatedNode;
 	mutable s32 LastMeshFrame;
