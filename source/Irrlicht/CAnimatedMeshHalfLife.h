b5c7c9695cc613ff8d48115cb7fba1995b335ae7
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2011-12-03 23:51:53 +0000

    Change interpolation mesh to pointer usage and dynamic memory allocation. Necessary due to reference counting.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@4006 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CAnimatedMeshHalfLife.h
+++ source/Irrlicht/CAnimatedMeshHalfLife.h
@@ -539,7 +539,7 @@ namespace scene
 		IBodyList BodyList;
 
 		//! return a Mesh per frame
-		SMesh MeshIPol;
+		SMesh* MeshIPol;
 
 		ISceneManager *SceneManager;
 
@@ -559,6 +559,7 @@ namespace scene
 		u8 BoneController[4 + 1 ]; // bone controllers + mouth position
 		u8 Blending[2]; // animation blending
 
+		vec4_hl BoneAdj;
 		f32 SetController( s32 controllerIndex, f32 value );
 
 		u32 SkinGroupSelection; // skin group selection
@@ -574,7 +575,6 @@ namespace scene
 		void slerpBones( vec4_hl q1[], vec3_hl pos1[], vec4_hl q2[], vec3_hl pos2[], f32 s );
 		void calcRotations ( vec3_hl *pos, vec4_hl *q, SHalflifeSequence *seq, SHalflifeAnimOffset *anim, f32 f );
 
-		vec4_hl BoneAdj;
 		void calcBoneAdj();
 		void calcBoneQuaternion(const s32 frame, const SHalflifeBone *bone, SHalflifeAnimOffset *anim, const u32 j, f32& angle1, f32& angle2) const;
 		void calcBonePosition(const s32 frame, f32 s, const SHalflifeBone *bone, SHalflifeAnimOffset *anim, f32 *pos ) const;
