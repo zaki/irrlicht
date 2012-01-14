c1528e11b1f424bb820a21c04b7ce6e3f69e6d97
bitplane <bitplane@dfc29bdd-3216-0410-991c-e03cc46cb475>
2012-01-14 16:15:26 +0000

    Fix for bug 3401933 - vertex color interpolation with shadow volumes in the scene
    git-svn-id: https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk@4041 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/COpenGLDriver.cpp
+++ source/Irrlicht/COpenGLDriver.cpp
@@ -3651,7 +3651,7 @@ void COpenGLDriver::drawStencilShadow(bool clearStencilBuffer, video::SColor lef
 	disableTextures();
 
 	// store attributes
-	glPushAttrib(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_ENABLE_BIT | GL_POLYGON_BIT | GL_STENCIL_BUFFER_BIT);
+	glPushAttrib(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_ENABLE_BIT | GL_POLYGON_BIT | GL_STENCIL_BUFFER_BIT | GL_LIGHTING_BIT);
 
 	glDisable(GL_LIGHTING);
 	glDisable(GL_FOG);
