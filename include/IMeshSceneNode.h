4797bbcaad75371a13ee2bedbed041003fa7a960
Nadro <Nadro@dfc29bdd-3216-0410-991c-e03cc46cb475>
2012-10-18 23:50:31 +0000

    - Fixed infinity default value for static meshes.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@4334 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IMeshSceneNode.h
+++ include/IMeshSceneNode.h
@@ -50,12 +50,14 @@ public:
 	\param zfailmethod: If set to true, the shadow will use the
 	zfail method, if not, zpass is used.
 	\param infinity: Value used by the shadow volume algorithm to
-	scale the shadow volume.
+	scale the shadow volume (for zfail shadow volume we support only
+	finite shadows, so camera zfar must be larger than shadow back cap,
+	which is depend on infinity parameter).
 	\return Pointer to the created shadow scene node. This pointer
 	should not be dropped. See IReferenceCounted::drop() for more
 	information. */
 	virtual IShadowVolumeSceneNode* addShadowVolumeSceneNode(const IMesh* shadowMesh=0,
-		s32 id=-1, bool zfailmethod=true, f32 infinity=10000.0f) = 0;
+		s32 id=-1, bool zfailmethod=true, f32 infinity=1000.0f) = 0;
 
 	//! Sets if the scene node should not copy the materials of the mesh but use them in a read only style.
 	/** In this way it is possible to change the materials of a mesh
