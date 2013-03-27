4c464ee0053d61de63e5c137b8ae4a5f18bb4464
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2013-03-27 18:04:12 +0000

    Add a test for particle system to exist before working with it
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@4486 dfc29bdd-3216-0410-991c-e03cc46cb475

--- examples/08.SpecialFX/main.cpp
+++ examples/08.SpecialFX/main.cpp
@@ -165,30 +165,33 @@ int main()
 	scene::IParticleSystemSceneNode* ps =
 		smgr->addParticleSystemSceneNode(false);
 
-	scene::IParticleEmitter* em = ps->createBoxEmitter(
-		core::aabbox3d<f32>(-7,0,-7,7,1,7), // emitter size
-		core::vector3df(0.0f,0.06f,0.0f),   // initial direction
-		80,100,                             // emit rate
-		video::SColor(0,255,255,255),       // darkest color
-		video::SColor(0,255,255,255),       // brightest color
-		800,2000,0,                         // min and max age, angle
-		core::dimension2df(10.f,10.f),         // min size
-		core::dimension2df(20.f,20.f));        // max size
+	if (ps)
+	{
+		scene::IParticleEmitter* em = ps->createBoxEmitter(
+			core::aabbox3d<f32>(-7,0,-7,7,1,7), // emitter size
+			core::vector3df(0.0f,0.06f,0.0f),   // initial direction
+			80,100,                             // emit rate
+			video::SColor(0,255,255,255),       // darkest color
+			video::SColor(0,255,255,255),       // brightest color
+			800,2000,0,                         // min and max age, angle
+			core::dimension2df(10.f,10.f),         // min size
+			core::dimension2df(20.f,20.f));        // max size
 
-	ps->setEmitter(em); // this grabs the emitter
-	em->drop(); // so we can drop it here without deleting it
+		ps->setEmitter(em); // this grabs the emitter
+		em->drop(); // so we can drop it here without deleting it
 
-	scene::IParticleAffector* paf = ps->createFadeOutParticleAffector();
+		scene::IParticleAffector* paf = ps->createFadeOutParticleAffector();
 
-	ps->addAffector(paf); // same goes for the affector
-	paf->drop();
+		ps->addAffector(paf); // same goes for the affector
+		paf->drop();
 
-	ps->setPosition(core::vector3df(-70,60,40));
-	ps->setScale(core::vector3df(2,2,2));
-	ps->setMaterialFlag(video::EMF_LIGHTING, false);
-	ps->setMaterialFlag(video::EMF_ZWRITE_ENABLE, false);
-	ps->setMaterialTexture(0, driver->getTexture("../../media/fire.bmp"));
-	ps->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+		ps->setPosition(core::vector3df(-70,60,40));
+		ps->setScale(core::vector3df(2,2,2));
+		ps->setMaterialFlag(video::EMF_LIGHTING, false);
+		ps->setMaterialFlag(video::EMF_ZWRITE_ENABLE, false);
+		ps->setMaterialTexture(0, driver->getTexture("../../media/fire.bmp"));
+		ps->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+	}
 
 	/*
 	Next we add a volumetric light node, which adds a glowing fake area light to
