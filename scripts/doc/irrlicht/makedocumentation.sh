94203cd075cddd9b6d1b0f50916db7103c1b08ef
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2014-01-14 11:00:06 +0000

    Linux version of makedocumentation.sh now works like the DOS version and creates html docs.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@4666 dfc29bdd-3216-0410-991c-e03cc46cb475

--- scripts/doc/irrlicht/makedocumentation.sh
+++ scripts/doc/irrlicht/makedocumentation.sh
@@ -1,10 +1,11 @@
 rm tut.txt || true;
+
+mkdir ../../../doctemp
+mkdir ../../../doctemp/html
+cp doxygen.css irrlicht.png logobig.png ../../../doctemp/html
+
 for i in ../../../examples/[012]*/main.cpp; do
   sed -f tutorials.sed $i >>tut.txt;
 done
 
-doxygen doxygen-pdf.cfg
-
-cp doxygen.css irrlicht.png logobig.png ../../../doctemp/html
-
-
+doxygen doxygen.cfg
