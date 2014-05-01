e487bcdc7253107be3c1ed65cd288775f0969d1a
hybrid <hybrid@dfc29bdd-3216-0410-991c-e03cc46cb475>
2014-05-01 19:11:32 +0000

    Merged new versions of libpng and jpeglib from vendor branch. Had to fix some minor things in JPG reader and the jconfig.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@4823 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/jpeglib/jconfig.h
+++ source/Irrlicht/jpeglib/jconfig.h
@@ -15,12 +15,13 @@
 #undef NEED_SHORT_EXTERNAL_NAMES
 #undef INCOMPLETE_TYPES_BROKEN
 
+#ifdef _MSC_VER
 /* Define "boolean" as unsigned char, not int, per Windows custom */
 #ifndef __RPCNDR_H__		/* don't conflict if rpcndr.h already read */
 typedef unsigned char boolean;
 #endif
 #define HAVE_BOOLEAN		/* prevent jmorecfg.h from redefining it */
-
+#endif
 
 #ifdef JPEG_INTERNALS
 
