ce863d1037108d4510e28454c9d5f1aa2bcc1704
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2015-04-11 16:08:54 +0000

    Update test drawVertexPrimitives - new media for OpenGL-drawVPL_h. This test is about finding out if things changed, and this result has been like it is now at least since 1.8 - required match can be reduce for this test, as minor aliasing artifacts always are different between cards and don't matter here.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@5090 dfc29bdd-3216-0410-991c-e03cc46cb475

--- tests/drawVertexPrimitive.cpp
+++ tests/drawVertexPrimitive.cpp
@@ -85,7 +85,7 @@ bool testWithDriver(video::E_DRIVER_TYPE driverType)
 		// we use character enumeration as we have more than 9 types
 		name.append(Type-scene::EPT_POINTS+'a');
 		name.append(".png");
-		result &= takeScreenshotAndCompareAgainstReference(driver, name.c_str());
+		result &= takeScreenshotAndCompareAgainstReference(driver, name.c_str(), 97.f);
 	}
 
 	device->closeDevice();
