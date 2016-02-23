2ed02b0b602f7a808c897529785cd78a0f0aa7fb
nadro <nadro@dfc29bdd-3216-0410-991c-e03cc46cb475>
2016-02-23 18:53:07 +0000

    - Fix for the previous commit.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@5266 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CD3D9Texture.cpp
+++ source/Irrlicht/CD3D9Texture.cpp
@@ -38,8 +38,12 @@ CD3D9Texture::CD3D9Texture(const io::path& name, const core::array<IImage*>& ima
 
 	core::array<IImage*> tmpImage = image;
 
+	bool releaseImageData = false;
+
 	if (OriginalSize != Size || OriginalColorFormat != ColorFormat)
 	{
+		releaseImageData = true;
+
 		for (u32 i = 0; i < image.size(); ++i)
 		{
 			tmpImage[i] = Driver->createImage(ColorFormat, Size);
@@ -103,9 +107,10 @@ CD3D9Texture::CD3D9Texture(const io::path& name, const core::array<IImage*>& ima
 		os::Printer::log("Could not create DIRECT3D9 Texture.", ELL_WARNING);
 	}
 	
-	for (u32 i = 0; i < tmpImage.size(); ++i)
+	if (releaseImageData)
     {
-        tmpImage[i]->drop();
+		for (u32 i = 0; i < tmpImage.size(); ++i)
+			tmpImage[i]->drop();
     }
 }
 
