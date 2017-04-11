41c8c324358c54a8e93bc86ec0812d89114789f2
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2017-04-11 13:30:25 +0000

    Avoid having to change IrrCompileConfig for emscripten - do set corresponding define in Makefile. Fix Makefile paths for emscripten.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/ogl-es@5409 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IrrCompileConfig.h
+++ include/IrrCompileConfig.h
@@ -109,7 +109,7 @@
 #define _IRR_COMPILE_WITH_EGL_MANAGER_
 #define _IRR_COMPILE_WITH_SDL_DEVICE_
 #define NO_IRR_COMPILE_WITH_X11_DEVICE_
-//#define _IRR_LINUX_PLATFORM_ //Hack
+//#define _IRR_LINUX_PLATFORM_ // defined in Makefile for emscripten
 #endif // __EMSCRIPTEN__
 
 #if defined(__ANDROID__)
