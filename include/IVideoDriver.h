4ee827b16a5a1dd1174ff8f637426c83b3ecfef6
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2017-08-23 16:41:15 +0000

    Move SOverrideMaterial to it's own header.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@5528 dfc29bdd-3216-0410-991c-e03cc46cb475

--- include/IVideoDriver.h
+++ include/IVideoDriver.h
@@ -13,12 +13,12 @@
 #include "plane3d.h"
 #include "dimension2d.h"
 #include "position2d.h"
-#include "SMaterial.h"
 #include "IMeshBuffer.h"
 #include "triangle3d.h"
 #include "EDriverTypes.h"
 #include "EDriverFeatures.h"
 #include "SExposedVideoData.h"
+#include "SOverrideMaterial.h"
 
 namespace irr
 {
@@ -154,71 +154,6 @@ namespace video
 		"FogLinear",
 		"FogExp2",
 		0
-	};
-
-	struct SOverrideMaterial
-	{
-		//! The Material values
-		SMaterial Material;
-		//! Which values are taken for override
-		/** OR'ed values from E_MATERIAL_FLAGS. */
-		u32 EnableFlags;
-		//! Set in which render passes the material override is active.
-		/** OR'ed values from E_SCENE_NODE_RENDER_PASS. */
-		u16 EnablePasses;
-		//! Global enable flag, overwritten by the SceneManager in each pass
-		/** The Scenemanager uses the EnablePass array and sets Enabled to
-		true if the Override material is enabled in the current pass. */
-		bool Enabled;
-
-		//! Default constructor
-		SOverrideMaterial() : EnableFlags(0), EnablePasses(0), Enabled(false) {}
-
-		//! Apply the enabled overrides
-		void apply(SMaterial& material)
-		{
-			if (Enabled)
-			{
-				for (u32 i=0; i<32; ++i)
-				{
-					const u32 num=(1<<i);
-					if (EnableFlags & num)
-					{
-						switch (num)
-						{
-						case EMF_WIREFRAME: material.Wireframe = Material.Wireframe; break;
-						case EMF_POINTCLOUD: material.PointCloud = Material.PointCloud; break;
-						case EMF_GOURAUD_SHADING: material.GouraudShading = Material.GouraudShading; break;
-						case EMF_LIGHTING: material.Lighting = Material.Lighting; break;
-						case EMF_ZBUFFER: material.ZBuffer = Material.ZBuffer; break;
-						case EMF_ZWRITE_ENABLE: material.ZWriteEnable = Material.ZWriteEnable; break;
-						case EMF_BACK_FACE_CULLING: material.BackfaceCulling = Material.BackfaceCulling; break;
-						case EMF_FRONT_FACE_CULLING: material.FrontfaceCulling = Material.FrontfaceCulling; break;
-						case EMF_BILINEAR_FILTER: material.TextureLayer[0].BilinearFilter = Material.TextureLayer[0].BilinearFilter; break;
-						case EMF_TRILINEAR_FILTER: material.TextureLayer[0].TrilinearFilter = Material.TextureLayer[0].TrilinearFilter; break;
-						case EMF_ANISOTROPIC_FILTER: material.TextureLayer[0].AnisotropicFilter = Material.TextureLayer[0].AnisotropicFilter; break;
-						case EMF_FOG_ENABLE: material.FogEnable = Material.FogEnable; break;
-						case EMF_NORMALIZE_NORMALS: material.NormalizeNormals = Material.NormalizeNormals; break;
-						case EMF_TEXTURE_WRAP:
-							material.TextureLayer[0].TextureWrapU = Material.TextureLayer[0].TextureWrapU;
-							material.TextureLayer[0].TextureWrapV = Material.TextureLayer[0].TextureWrapV;
-							material.TextureLayer[0].TextureWrapW = Material.TextureLayer[0].TextureWrapW;
-							break;
-						case EMF_ANTI_ALIASING: material.AntiAliasing = Material.AntiAliasing; break;
-						case EMF_COLOR_MASK: material.ColorMask = Material.ColorMask; break;
-						case EMF_COLOR_MATERIAL: material.ColorMaterial = Material.ColorMaterial; break;
-						case EMF_USE_MIP_MAPS: material.UseMipMaps = Material.UseMipMaps; break;
-						case EMF_BLEND_OPERATION: material.BlendOperation = Material.BlendOperation; break;
-						case EMF_BLEND_FACTOR: material.BlendFactor = Material.BlendFactor; break;
-						case EMF_POLYGON_OFFSET:
-							material.PolygonOffsetDirection = Material.PolygonOffsetDirection;
-							material.PolygonOffsetFactor = Material.PolygonOffsetFactor; break;
-						}
-					}
-				}
-			}
-		}
-
 	};
 
 	//! Interface to driver which is able to perform 2d and 3d graphics functions.
