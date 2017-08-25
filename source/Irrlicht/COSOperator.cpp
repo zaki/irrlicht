c937fcf488f55a97b36ed12bb4557c1552545d91
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2017-08-25 15:01:47 +0000

    Fix getProcessorSpeedMHz on Linux. Thanks @devsh for mentioning problems in that area. Note that the MHz can vary each second and is only returned for first processor.
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/trunk@5534 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/COSOperator.cpp
+++ source/Irrlicht/COSOperator.cpp
@@ -180,7 +180,7 @@ bool COSOperator::getProcessorSpeedMHz(u32* MHz) const
 		*MHz = CpuClock.hz;
 	return true;
 #else
-	// could probably be read from "/proc/cpuinfo" or "/proc/cpufreq"
+	// read from "/proc/cpuinfo"
 	FILE* file = fopen("/proc/cpuinfo", "r");
 	if (file)
 	{
@@ -194,7 +194,8 @@ bool COSOperator::getProcessorSpeedMHz(u32* MHz) const
 			pos = str.findNext(':', pos);
 			if (pos != -1)
 			{
-				*MHz = core::fast_atof(str.c_str()+pos+1);
+				while ( str[++pos] == ' ' );
+				*MHz = core::fast_atof(str.c_str()+pos);
 			}
 		}
 		fclose(file);
